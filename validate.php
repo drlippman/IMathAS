675957f22c339812c0ccafc7a9a1baedc2af0b0b
drlippman <drlippman@c89b4f0b-ac2a-0410-9773-c9071ee4f95d>
2008-10-13 23:14:13 +0000

    added validate check to keep diag taker in diag
    git-svn-id: http://imathas.googlecode.com/svn/trunk@254 c89b4f0b-ac2a-0410-9773-c9071ee4f95d

--- validate.php
+++ validate.php
@@ -198,6 +198,9 @@ END;
 	$previewshift = -1;
 	$coursetheme = "default.css";
 	$basephysicaldir = rtrim(dirname(__FILE__), '/\\');
+	if (isset($sessiondata['isdiag']) && strpos(basename($_SERVER['PHP_SELF']),'showtest.php')===false) {
+		header("Location: http://" . $_SERVER['HTTP_HOST'] . $imasroot . "/assessment/showtest.php");
+	}
 	if (isset($sessiondata['ltiitemtype'])) {
 		if ($sessiondata['ltiitemtype']==1) {
 			if (strpos(basename($_SERVER['PHP_SELF']),'showtest.php')===false && isset($_GET['cid']) && $sessiondata['ltiitemid']!=$_GET['cid']) {
