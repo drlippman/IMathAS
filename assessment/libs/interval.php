bae55f54433ce4b7ab10f56c661db5918d956e86
drlippman <drlippman@c89b4f0b-ac2a-0410-9773-c9071ee4f95d>
2011-10-21 17:30:31 +0000

    add alt form of linegraph to interval macro library
    git-svn-id: http://imathas.googlecode.com/svn/trunk@767 c89b4f0b-ac2a-0410-9773-c9071ee4f95d

--- assessment/libs/interval.php
+++ assessment/libs/interval.php
@@ -2,7 +2,7 @@
 //Interval functions, Version 1.1 March 2, 2010
 
 global $allowedmacros;
-array_push($allowedmacros,"linegraph","forminterval","intervalstodraw");
+array_push($allowedmacros,"linegraph","linegraphbrackets","forminterval","intervalstodraw");
 
 //forminterval(min,max,minmarkertype,maxmarkertype)
 //Forms an interval string, like "[2,3)"
@@ -101,6 +101,82 @@ function linegraph($intvs) {
 	}
 	
 }
+
+//linegraphbrackets(intervals,[xmin,xmax,xscl,width,height])
+//Creates a line graph using brackets/parens instead of open/closed dots
+//intervals:  string or array of strings in interval notation, ex: [2,5)
+function linegraphbrackets($intvs) {
+	if (!is_array($intvs)) {
+		settype($intvs,"array");
+	}
+	$settings = array(-5,5,1,300,70);
+	for ($i = 1; $i < func_num_args(); $i++) {
+		$settings[$i-1] = func_get_arg($i);
+	}
+	
+	$commands = "setBorder(5); initPicture({$settings[0]},{$settings[1]},-.5,.5);";
+	$alt = "Line Graph, x {$settings[0]} to {$settings[1]}.";
+	$commands .= 'axes('.$settings[2].',1,1,0,0,1,"off"); stroke="blue"; strokewidth=2;';
+	foreach ($intvs as $intv) {
+		$commands .= 'line([';
+		$intv = str_replace(' ','',$intv);
+		$parts = explode(',',$intv);
+		$ssym = $parts[0]{0};
+		$min = substr($parts[0],1);
+		$esym = substr($parts[1],-1);
+		$max = substr($parts[1],0,strlen($parts[1])-1);
+		if ($min=='-oo' || $min<$settings[0]) {
+			$commands .= $settings[0];
+		} else {
+			$commands .= $min;
+		}
+		$commands .= ',0],[';
+		if ($max=='oo' || $max=='+oo' || $max>$settings[1]) {
+			$commands .= $settings[1];
+		} else {
+			$commands .= $max;
+		}
+		$commands .= ',0]);';
+		$alt .= "Line from $min to $max";
+		if ($min=='-oo' || $min<$settings[0]) {
+			$offlitt = $settings[0] + .1;
+			$commands .= 'marker="arrow";';
+			$commands .= "line([$offlitt,0],[{$settings[0]},0]);";
+			$commands .= 'marker="none";';
+		} else {
+			$x = $min+.1;
+			if ($ssym=='[') {
+				$commands .= "path([[$x,.1],[$min,.1],[$min,-.1],[$x,-.1]]);";
+				$alt .= ", left bracket at $min";
+			} else {
+				$commands .= "arc([$x,.1],[$x,-.1],.112);";
+				$alt .= ", left parenthesis at $min";
+			}
+		}
+		if ($max=='oo' || $max=='+oo' || $max>$settings[1]) {
+			$offlitt = $settings[1] - .1;
+			$commands .= 'marker="arrow";';
+			$commands .= "line([$offlitt,0],[{$settings[1]},0]);";
+			$commands .= 'marker="none";';
+		} else {
+			$x = $max-.1;
+			if ($esym==']') {
+				$commands .= "path([[$x,.1],[$max,.1],[$max,-.1],[$x,-.1]]);";
+				$alt .= ", right bracket at $max";
+			} else {
+				$commands .= "arc([$x,-.1],[$x,.1],.112);";
+				$alt .= ", right parenthesis at $max";
+			}
+		}
+	}
+	if ($GLOBALS['sessiondata']['graphdisp']==0) {
+		return $alt;
+	} else {
+		return "<embed type='image/svg+xml' align='middle' width='$settings[3]' height='$settings[4]' src='{$GLOBALS['imasroot']}/javascript/d.svg' script='$commands' />\n";
+	}
+	
+}
+
 
 //intervalstodraw(interval,xmin,xmax)
 //converts an interval like (-oo,4]U(3,5] into an array of strings 
