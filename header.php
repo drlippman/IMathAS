d2b54e89cd9e308da457c9416e0f2fbe3e1279a8
drlippman <drlippman@c89b4f0b-ac2a-0410-9773-c9071ee4f95d>
2012-01-14 01:22:43 +0000

    Add attachment editor plugin
    git-svn-id: http://imathas.googlecode.com/svn/trunk@839 c89b4f0b-ac2a-0410-9773-c9071ee4f95d

--- header.php
+++ header.php
@@ -32,7 +32,7 @@ div.breadcrumb { display:none;}
 <script type="text/javascript">
 var imasroot = '<?php echo $imasroot; ?>';
 </script>
-<script type="text/javascript" src="<?php echo $imasroot;?>/javascript/general.js?ver=111011"></script>
+<script type="text/javascript" src="<?php echo $imasroot;?>/javascript/general.js?ver=011312"></script>
 <?php
 if (isset($coursetheme) && strpos($coursetheme,'_dark')!==false) {$mathdarkbg = true;} else {$mathdarkbg = false;}
 if (isset($ispublic) && $ispublic) {
