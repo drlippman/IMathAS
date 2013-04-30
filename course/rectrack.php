7c2db6a764b372525f45321144a55b5994cb3ad9
David Lippman <drlippman@gmail.com>
2013-04-30 15:46:11 -0700

    Add rectrack.php file for recording content tracking

---
+++ course/rectrack.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Types:
+inlinetext	In inline text summary		imas_inlinetext.id
+linkedsum	In linked text summary		imas_linkedtext.id
+linkedlink	Linked text main link		imas_linkedtext.id
+linkedintext	In Linked text test		imas_linkedtext.id
+linkedviacal	Linked text via Calendar	imas_linkedtext.id
+extref		Button click in question	imas_questions.id
+assessintro	Link in assessment intro	imas_assessments.id
+assesssum	Link in assessment summary	imas_assessments.id
+wiki		Link to wiki			imas_wikis.id
+wikiintext	Link in wiki text		imas_wikis.id
+*/
+
+require("../validate.php");
+if (isset($studentid)) {
+	$now = time();
+	$query = "INSERT INTO imas_content_track (userid,courseid,type,typeid,viewtime,info) VALUES ";
+	$query .= "('$userid','$cid','{$_POST['type']}','{$_POST['typeid']}',$now,'{$_POST['info']}')";
+	mysql_query($query) or die("Query failed : " . mysql_error());
+} 
+?>
