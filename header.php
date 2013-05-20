549a567ca4bd55feb49cda792dea40ae5e10aac0
drlippman <drlippman@gmail.com>
2013-05-19 18:16:50 -0700

    Fix cache reload for imascore after last update Add support for autoenroll courses to newinstructor

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=7, IE=Edge" />
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" type="text/javascript"></script>
-<link rel="stylesheet" href="<?php echo $imasroot . "/imascore.css?ver=112111";?>" type="text/css" />
+<link rel="stylesheet" href="<?php echo $imasroot . "/imascore.css?ver=042313";?>" type="text/css" />
 <?php if (isset($coursetheme)) { 
 	if (isset($flexwidth) || isset($usefullwidth)) {
 		$coursetheme = str_replace('_fw','',$coursetheme);
