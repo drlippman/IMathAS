fad6782d2a17f4155bbed21b18b61f55005e471a
David Lippman <drlippman@gmail.com>
2014-01-29 14:07:57 -0800

    Bug fix mathquill not working in early IE due to use of trim.

--- javascript/mathquill.js
+++ javascript/mathquill.js
@@ -290,7 +290,7 @@ var manageTextarea = (function() {
     }
 
     function popText(callback) {
-      var text = textarea.val().trim();
+      var text = jQuery.trim(textarea.val());
       textarea.val(' ');
       if (text) callback(text);
     }
