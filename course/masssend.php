1e3257b6a30d8420b09f60f087559c912debb16c
drlippman <drlippman@yahoo.com>
2014-10-01 16:36:24 -0700

    Revert email cc from sendfrom Fix composition symbol in mathjax

--- course/masssend.php
+++ course/masssend.php
@@ -131,9 +131,9 @@
 				}
 				$message .= "<p>A copy was also emailed to all instructors for this course</p>\n";
 			}
-			$headers  = 'MIME-Version: 1.0' . "\r\n";
-			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-			$headers .= "From: $sendfrom\r\n";
+			//$headers  = 'MIME-Version: 1.0' . "\r\n";
+			//$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+			//$headers .= "From: $sendfrom\r\n";
 			foreach ($teacheraddys as $addy) {
 				mail($addy,$subject,$message,$headers);
 			}
