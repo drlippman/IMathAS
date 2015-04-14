ab9cceb73747c368c57fbe86be809612eb8ec091
Abhishek Prajapati <abhishek.prajapati@tudip.com>
2015-04-14 23:35:46 +0530

    Feature #19734: Remaining files of previous commit.

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -19,7 +19,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        '/css/imascore.css?ver=030415',
+        'css/imascore.css?ver=030415',
         'css/default.css?v=121713',
         'css/handheld.css',
     ];
