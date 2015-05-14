2f64791b7fe3411ebf642b1c98295b50cda858fb
Sumit Joshi <sumit_joshi@tudip.com>
2015-05-14 11:49:22 +0530

    Feature #20363: Teacher Messages: Sent Message  - Created a view for "sent message" page.  - Added top bar on the same page.  - Top bar subsumes course, messages, forum, roster, calendar, gradebook.  - Added a hyper link for "sent messages option" on message index page.  - When instructor clicks "sent messages" link, redirected to "sent message" page.  - Created drop-down list to display "Filter by courses".  - Created drop-down list to to display "Filter by recipient".  - Created "check all" and "check none" hyper link to check and uncheck the checkboxes.  - Created a button to remove the sent message from the table.  - Added a data table to display all the sent messages from the database.  - Add a hyper link for received message option.  - When clicked on the received message link user will be redirected to message index page.

--- views/message/message/sentMessage.php
+++ views/message/message/sentMessage.php
@@ -2,14 +2,14 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use app\components\AppUtility;
-
-$this->title = 'Messages';
+$this->title = 'Sent Messages';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <!-- DataTables CSS -->
+
     <link rel="stylesheet" type="text/css"
           href="<?php echo AppUtility::getHomeURL() ?>js/DataTables-1.10.6/media/css/jquery.dataTables.css">
     <script type="text/javascript" src="<?php echo AppUtility::getHomeURL() ?>js/general.js?ver=012115"></script>
@@ -18,48 +18,46 @@ $this->params['breadcrumbs'][] = $this->title;
             src="<?php echo AppUtility::getHomeURL() ?>js/DataTables-1.10.6/media/js/jquery.dataTables.js"></script>
     <script type="text/javascript" charset="utf8"
             src="<?php echo AppUtility::getHomeURL() ?>js/DataTables-1.10.6/media/js/jquery.dataTables.js"></script>
+
+
 </head>
 <body>
 <div>
     <?php echo $this->render('../../instructor/instructor/_toolbarTeacher'); ?>
-    <input type="hidden" class="send-msg" value="<?php echo $course->id ?>">
-    <input type="hidden" class="send-userId" value="<?php echo $course->ownerid ?>">
+    <input type="hidden" class="send-course-id" value="<?php echo $course->id ?>">
+    <input type="hidden" class="send-user-id" value="<?php echo $course->ownerid ?>">
 </div>
 <div class="message-container">
-<div><p><a href="<?php echo AppUtility::getURLFromHome('message', 'message/send-message?cid='.$course->id.'&userid='.$course->ownerid); ?>" class="btn btn-primary ">Send New Message</a>
-    | <a href="">Limit to Tagged</a> | <a href="<?php echo AppUtility::getURLFromHome('message', 'message/sent-message?cid='.$course->id.'&userid='.$course->ownerid); ?>">Sent Messages</a>
-    | <a class="btn btn-primary ">Picture</a></p>
-</div>
-<div>
-    <p><span class="col-md-2" align="center"><b>Filter By Course :</b></span>
+    <div><p><a href="<?php echo AppUtility::getURLFromHome('message', 'message/index?cid='.$course->id); ?>">Received Messages</a></p>
+    </div>
+    <div>
+        <p><span class="col-md-2" align="center"><b>Filter By Courses :</b></span>
         <span class="col-md-3">
         <select name="seluid" class="dropdown form-control" id="seluid">
             <option value="0">All Courses</option>
 
         </select>
 
-        </span> <span class="col-md-2" align="center"><b>By Sender :</b></span>
+        </span> <span class="col-md-2" align="center"><b>By Recipient :</b></span>
 
         <span class="col-md-3">
         <select name="seluid" class="dropdown form-control" id="seluid">
             <option value="0">Select a user..</option>
-            <?php foreach ($users as $user) { ?>
-                <option
-                    value="<?php echo $user['id'] ?>"><?php echo $user['FirstName'] . " " . $user['LastName']; ?></option>
-            <?php } ?>
+
+            <!-- script is missing -->
+
         </select>
 
-
-
         </span></p>
-</div><br><br>
+    </div><br><br>
     <div>
         <p>check: <a id="uncheck-all-box" class="uncheck-all" href="#">None</a> /
             <a id="check-all-box" class="check-all" href="#">All</a>
             With Selected:
-            <a class="btn btn-primary ">Mark as Unread</a>
-            <a class="btn btn-primary ">Mark as Read</a>
-            <a class="btn btn-primary ">Delete</a>
+            <a class="btn btn-primary ">Remove From Sent Message List</a>
+            <a class="btn btn-primary ">Unsend</a>
+        </p>
+
     </div>
 
     <table id="message-table display-message-table" class="message-table display-message-table">
@@ -67,10 +65,8 @@ $this->params['breadcrumbs'][] = $this->title;
         <tr>
             <th></th>
             <th>Message</th>
-            <th>Replied</th>
-            <th>Flag</th>
-            <th>From</th>
-            <th>Course</th>
+            <th>To</th>
+            <th>Read</th>
             <th>Sent</th>
         </tr>
         </thead>
@@ -80,18 +76,19 @@ $this->params['breadcrumbs'][] = $this->title;
 </div>
 </body>
 </html>
+
+<!-- Script for table is remaining-->
 <script type="text/javascript">
     $(document).ready(function () {
-        var cid = $(".send-msg").val();
-        var userId = $(".send-userId").val();
-        alert(userId);
-        var allMessage = {cid: cid, userId: userId};
-        jQuerySubmit('display-message-ajax',allMessage, 'showMessageSuccess');
+        var cid = $(".send-course-id").val();
+        var userId = $(".send-user-id").val();
+        var inputData = {cid: cid, userId: userId};
+        jQuerySubmit('display-sent-message-ajax',inputData, 'showMessageSuccess');
         selectCheckBox();
     });
 
     function showMessageSuccess(response)
-    {console.log(response);
+    {
         var result = JSON.parse(response);
         if(result.status == 0)
         {
@@ -106,10 +103,8 @@ $this->params['breadcrumbs'][] = $this->title;
         $.each(messageData, function(index, messageData){
             html += "<tr> <td><input type='checkbox' name='msg-check' value='"+messageData.id+"' class='message-checkbox-"+messageData.id+"' ></td>";
             html += "<td>"+messageData.title+"</td>";
-            html += "<td>"+messageData.replied+"</td>";
-            html += "<td>abc</td>";
-            html += "<td>"+messageData.msgFrom+"</td>";
-            html += "<td>"+messageData.courseName+"</td>";
+            html += "<td>"+messageData.msgTo+"</td>";
+            html += "<td>"+messageData.isRead+"</td>";
             html += "<td>"+messageData.msgDate+"</td>";
         });
         $(".message-table-body").append(html);
@@ -129,6 +124,5 @@ $this->params['breadcrumbs'][] = $this->title;
             })
         });
     }
-
 
 </script>
