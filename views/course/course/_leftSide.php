3a53f85604d31bbf8f68332a69daa105e2a6895a
Pradeep Pandey <pradeep.pandey@tudip.nl>
2015-05-21 22:52:52 +0530

    Feature #20653: Student Assessment: If assessment set password.  - Show assessment name of selected assessment by student on view.  - Created label and text-field for password on view.  - Created button on view for submitting password.  - When student will enter password, it will check with assessment table for password.  - If both are matched, it will show success flash message.  - If it is not match with assessment table's password, it will show error message.

--- views/course/course/_leftSide.php
+++ views/course/course/_leftSide.php
@@ -4,7 +4,7 @@ use app\components\AppUtility;
 ?>
 
 <p>
-    <a href=""> Messages</a><br>
+    <a href="<?php echo AppUtility::getURLFromHome('message', 'message/index?cid='.$course->id); ?>"> Messages</a><br>
     <a href="<?php echo AppUtility::getURLFromHome('forum', 'forum/search-forum?cid='.$course->id); ?>"> Forums</a><br>
     <a href="">Calendar</a><br>
     <a href="">Log Out</a><br>
