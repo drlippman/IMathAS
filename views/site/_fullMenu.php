e7fcf638e974fbbdb86df6cf7b2c904d24e9b4b2
Abhishek Prajapati <abhishek.prajapati@tudip.com>
2015-05-28 17:34:14 +0530

    General commit: Added hyper links on admin dashboard

--- views/site/_fullMenu.php
+++ views/site/_fullMenu.php
@@ -5,6 +5,6 @@ use app\components\AppUtility;
 <div class="floatright" id="homelinkbox">
     <a href="<?php echo AppUtility::getURLFromHome('site', 'change-user-info') ?>">Change User Info</a> |
     <a href="<?php echo AppUtility::getURLFromHome('site', 'change-password') ?>">Change Password</a> |
-    <a href="<?php echo AppUtility::getURLFromHome('site', 'messages') ?>">Messages</a> |
-    <a href="<?php echo AppUtility::getURLFromHome('site', 'documentation') ?>">Documentation</a>
+    <a href="<?php echo AppUtility::getURLFromHome('site', 'work-in-progress?id=') ?>">Messages</a> |
+    <a href="<?php echo AppUtility::getURLFromHome('site', 'work-in-progress?id=') ?>">Documentation</a>
 </div>
--- views/site/_guestMenu.php
+++ views/site/_guestMenu.php
@@ -3,5 +3,5 @@ use app\components\AppUtility;
 ?>
 
 <div class="floatright" id="homelinkbox">
-    <a href="<?php echo AppUtility::getURLFromHome('site', 'messages') ?>">Messages</a>
+    <a href="<?php echo AppUtility::getURLFromHome('site', 'work-in-progress?id=') ?>">Messages</a>
 </div>
