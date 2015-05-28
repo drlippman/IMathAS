17dc826ce24c4cd53616ec55197819ebfd7196b3
Abhishek Prajapati <abhishek.prajapati@tudip.com>
2015-05-28 12:17:05 +0530

    Feature #20689: Teacher Roster: Import Student from file.  - Added form for import student from file.

---
+++ models/forms/ImportStudentForm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app\models\forms;
+
+use app\components\AppUtility;
+use app\components\AppConstant;
+use Yii;
+use yii\base\Model;
+use app\controllers\roster\RosterController;
+
+class ImportStudentForm extends Model
+{
+    public $file;
+    public $headerRow;
+    public $firstName;
+    public $nameFirstColumn;
+    public $lastName;
+    public $nameLastColumn;
+    public $emailAddress;
+    public $userName;
+    public $setPassword;
+    public $codeNumber;
+    public $sectionValue;
+    public $enrollStudent;
+
+    /**
+     * @return array the validation rules.
+     */
+    public function rules()
+    {
+        return [
+            ['file', 'safe'],
+            [['file'], 'file'],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'file' => 'Import File',
+            'headerRow' => 'File contains a header row',
+            'firstName' => 'First name is in column',
+            'nameFirstColumn' => 'In that column, first name is',
+            'lastName' => 'Last name is in column',
+            'nameLastColumn' => 'In that column, last name is',
+            'emailAddress' => 'Email address is in column:Enter 0 if no email column',
+            'userName' => 'Does a column contain a desired username:',
+            'setPassword' => 'Set password to',
+            'codeNumber' => 'Assign code number?',
+            'sectionValue' => 'Assign section value?',
+            'enrollStudent' => 'Enroll students in',
+        ];
+    }
+}
