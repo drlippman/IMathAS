aaa2ea15dd816d71dc1003dcffebbf719b3c8def
Pradeep Pandey <pradeep.pandey@tudip.nl>
2015-06-03 15:19:58 +0530

    Bug #20592: Reset password page issues.  - Aligned the Submit button properly.  - Redirected user to dashboard after password is successfully changed.

--- views/site/resetPassword.php
+++ views/site/resetPassword.php
@@ -10,8 +10,8 @@ $this->params['breadcrumbs'][] = $this->title;
         'id' => 'login-form',
         'options' => ['class' => 'form-horizontal'],
         'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-7 col-lg-offset-2\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-2 control-label'],
+            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8 col-lg-offset-2\">{error}</div>",
+            'labelOptions' => ['class' => 'col-lg-2'],
         ],
     ]); ?>
     <?= $form->field($model, 'newPassword')->passwordInput() ?>
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="form-group">
         <div class="col-lg-offset-2 col-lg-8 display_field">
-            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'resetpassword-button']) ?>
+            <?= Html::submitButton('Submit', ['id' =>'reset-password-submit','class' => 'btn btn-primary', 'name' => 'resetpassword-button']) ?>
         </div>
     </div>
 
