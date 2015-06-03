5ff47b272d437918e75d22c04a4a2303038a37a4
Sumit Joshi <sumit_joshi@tudip.com>
2015-06-03 14:45:20 +0530

    General Commit

--- views/admin/admin/helpOfRights.php
+++ views/admin/admin/helpOfRights.php
@@ -1,23 +1,23 @@
 
 <div class=h3>
-<h2 style="color: #000099">OpenMath Help</h2>
-<h3 style="color: #000099">Rights</h3>
+<h3 style="color: #000099">OpenMath Help</h3>
+<h4 style="color: #000099">Rights</h4>
 
 <h5>When adding a new administrator or changing rights, there are several rights levels, each higher level including the rights of the lower levels:
 </h5>
-    <h5>1.<b>Guest</b>: Can access all class materials, including taking tests (however, test is restarted next time the guest user accesses the test).Cannot enroll or unenroll in courses, or change
-       user info or password.  Cannot post in forums</h5>
-    <h5>2.<b>Student</b>: Can only access class materials - cannot edit anything</h5>
-    <h5>3.<b>Teacher</b>: Can edit course materials and create assessments, but only in courses
-	to which they have been assigned as a teacher</h5>
-    <h5>4.<b>Limited Course Creator</b>: Can add courses, and they are automatically assigned as the teacher.  Can delete courses
-       that they create.</h5>
-    <h5>5.<b>Diagnostic Creator</b>: Can add courses, and they are automatically assigned as the teacher.  Can delete courses
-       that they create.  Can create diagnostics.</h5>
-    <h5>6.<b>Group Admin</b>: Can add/delete teachers and set user rights, but only for users in their group.  Can modify/delete questions
-and libraries created by members of the group regardless of ownership or use rights.  Can always create "open to all" libraries</h5>
-    <h5>7.<b>Full Administrator</b>: Can add/delete administrators and set user rights.  Can import macro files, if installation allows.
-Can modify/delete questions and libraries regardless of ownership or use rights</h5>
+    <ol><h5><li><b>Guest</b>: Can access all class materials, including taking tests (however, test is restarted next time the guest user accesses the test).Cannot enroll or unenroll in courses, or change
+       user info or password.  Cannot post in forums.</li>
+    <li><b>Student</b>: Can only access class materials - cannot edit anything.</li>
+    <li><b>Teacher</b>: Can edit course materials and create assessments, but only in courses
+	to which they have been assigned as a teacher.</li>
+    <li><b>Limited Course Creator</b>: Can add courses, and they are automatically assigned as the teacher.  Can delete courses
+       that they create.</li>
+    <li><b>Diagnostic Creator</b>: Can add courses, and they are automatically assigned as the teacher.  Can delete courses
+       that they create.  Can create diagnostics.</li>
+    <li><b>Group Admin</b>: Can add/delete teachers and set user rights, but only for users in their group.  Can modify/delete questions
+and libraries created by members of the group regardless of ownership or use rights.  Can always create "open to all" libraries.</li>
+    <li><b>Full Administrator</b>: Can add/delete administrators and set user rights.  Can import macro files, if installation allows.
+Can modify/delete questions and libraries regardless of ownership or use rights.</li></h5><ol>
 
 
 </div>
