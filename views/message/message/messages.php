a59ce35664af777dd5c2713d0913074b40a71125
Pradeep Pandey <pradeep.pandey@tudip.nl>
2015-06-12 16:08:36 +0530

    Feature #20892: Student forum: Show flag read.  - Handled the exceptions.

--- views/message/message/messages.php
+++ views/message/message/messages.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <script type="text/javascript" charset="utf8"
         src="<?php echo AppUtility::getHomeURL() ?>js/DataTables-1.10.6/media/js/jquery.dataTables.js"></script>
 <div>
-    <?php if ($userRights->rights == 20) { ?>
+    <?php if ($userRights->rights > 10) { ?>
         <?php echo $this->render('../../instructor/instructor/_toolbarTeacher'); ?>
         <input type="hidden" class="send-msg" value="<?php echo $course->id ?>">
         <input type="hidden" class="send-userId" value="<?php echo $course->ownerid ?>">
