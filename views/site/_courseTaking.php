347851397ca73b76730d3d69319060475c31ca19
Abhishek Prajapati <abhishek.prajapati@tudip.com>
2015-06-16 12:37:53 +0530

    Updater: Shown the link to hide the course from student dashboard.  - Shown unhide link bellow the enroll button.

--- views/site/_courseTaking.php
+++ views/site/_courseTaking.php
@@ -1,5 +1,6 @@
 <?php
 use app\components\AppUtility;
+$isCourseHidden = false;
 ?>
 <div id="homefullwidth">
     <div class="block">
@@ -10,11 +11,18 @@ use app\components\AppUtility;
             <?php
             foreach ($students as $student) {
                 if ($student) {
-                    ?>
-                    <li>
-                        <a href="<?php echo AppUtility::getURLFromHome('course', 'course/index?cid=' . $student->courseid) ?>"><?php echo isset($student->course['name']) ? ucfirst($student->course['name']) : ""; ?></a>
-                    </li>
-                <?php
+                    if(!$student->hidefromcourselist){
+                        ?>
+                        <li>
+                            <span class="delx" onclick="return hidefromcourselist(this, $student->courseid);" title="Hide from course list">x</span>
+                            <a href="<?php echo AppUtility::getURLFromHome('course', 'course/index?cid=' . $student->courseid) ?>"><?php echo isset($student->course['name']) ? ucfirst($student->course['name']) : ""; ?></a>
+                        </li>
+                    <?php
+                    }
+                    elseif($student->hidefromcourselist)
+                    {
+                        $isCourseHidden = true;
+                    }
                 }
             }
             ?>
@@ -22,6 +30,13 @@ use app\components\AppUtility;
         <div class="center">
             <a class="btn btn-primary" href="<?php echo AppUtility::getURLFromHome('student', 'student/student-enroll-course') ?>">Enroll in a New Class</a><br>
             <a  id="unhidelink" class="course-taking small" href="work-in-progress">Unhide hidden courses</a>
+            <?php
+            if($isCourseHidden){
+            ?>
+                <input type="hidden" class="hidden-course" value="<?php echo $isCourseHidden ?>">
+            <?php
+            }
+            ?>
         </div>
     </div>
 </div>
