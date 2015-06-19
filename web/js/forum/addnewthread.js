49bdb7e480b1f058b1fc79a4e3770f7e39a0ef45
Pradeep Pandey <pradeep.pandey@tudip.nl>
2015-06-19 11:04:42 +0530

    Small fixes:Changed the editor in addnewthread.js

--- web/js/forum/addnewthread.js
+++ web/js/forum/addnewthread.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-    tinymce.init({
-        selector: "textarea",
-        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-    });
+    initEditor();
     $("#addNewThread").click(function()
     {
 
