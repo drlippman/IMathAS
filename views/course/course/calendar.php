689a3c7039bc4afe7e2035ca7d48be234962f8f4
Pradeep Pandey <pradeep.pandey@tudip.nl>
2015-07-18 20:53:34 +0530

    small fixes

--- views/course/course/calendar.php
+++ views/course/course/calendar.php
@@ -14,7 +14,7 @@ $currentDate = AppUtility::parsedatetime(date('m/d/Y'), date('h:i a'));
     <?php echo $this->render("../../instructor/instructor/_toolbarTeacher", ['course' => $course, 'section' => 'calendar']);?>
 
     <div class="tab-content col-lg-12">
-        <div class="col-lg-12 padding-alignment">
+        <div class="col-lg-12 padding-alignment calendar-container">
             <div class ='calendar padding-alignment calendar-alignment col-lg-9 pull-left'>
                 <input type="hidden" class="current-time" value="<?php echo $currentDate?>">
                 <div id="demo" style="display:table-cell; vertical-align:middle;"></div>
