ca3c238f68e731e3c39ea9ae89d4d2d7038a1c2c
priyankachaudhari-tudip <priyanka.chaudhari@tudip.nl>
2015-09-17 15:49:31 +0530

    Feature #23043: Full Admin: External tools: delete   - Applied master layout on it.   - Handled condition for name of external tool.   - Implemented a logic to remove exception on clicks of "Delete" button.

--- views/admin/admin/externalTool.php
+++ views/admin/admin/externalTool.php
@@ -54,9 +54,10 @@ else{
             <input type="hidden" id="name-external-tool" value="<?php echo $extName;?> ">
 
          <?php
-            echo '<p>Are you SURE you want to delete the tool <b>'.$extName.'</b>?  Doing so will break ALL placements of this tool.</p>';
+            echo '<br/>';
+            echo '<div class="col-lg-12">Are you SURE you want to delete the tool <b>'.$extName.'</b>?  Doing so will break ALL placements of this tool.</div><br/><br/>';
             echo '<form method="post" action="external-tool?cid='.$courseId.$ltfrom.'&amp;id='.$params['id'].'&amp;delete=true">';
-            echo '<input type=submit value="Yes, I\'m Sure">';
+            echo '<div class="col-lg-2"><input type=submit value="Yes, I\'m Sure"></div>';
             echo '</form>';
 
         } else if (isset($params['id'])) {
