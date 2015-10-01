c9fa382bfdc3dc1c0bf88ad104f0afd372af814f
Abhishek Prajapati <abhishek.prajapati@tudip.com>
2015-10-02 00:22:43 +0530

    Feature #21931: Display roster data table.  -Applied style to the Manage dropdown.  -Applied style to the Enroll Students dropdown.  -Fix the issue of dropdown when we click on dropdown.

--- views/roster/roster/_toolbarRoster.php
+++ views/roster/roster/_toolbarRoster.php
@@ -7,7 +7,7 @@ use app\components\AppUtility;
         <li><a href="<?php echo AppUtility::getURLFromHome('roster/roster', 'login-grid-view?cid='.$course->id); ?>"><?php AppUtility::t('View Login Grid');?></a></li>
         <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php AppUtility::t('Manage');?><span class="caret"></span></a>
-            <ul class="dropdown-menu settings-menu">
+            <ul class="dropdown-menu settings-menu min-with-hundred-per">
                 <li><a href="<?php echo AppUtility::getURLFromHome('roster/roster', 'assign-sections-and-codes?cid='.$course->id); ?>"><?php AppUtility::t('Sections and codes');?></a></li>
                 <li><a href="<?php echo AppUtility::getURLFromHome('roster/roster', 'manage-late-passes?cid='.$course->id); ?>"><?php AppUtility::t('LatePass');?></a></li>
                 <li><a href="<?php echo AppUtility::getURLFromHome('roster/roster', 'manage-tutors?cid='.$course->id); ?>"><?php AppUtility::t('Tutors');?></a></li>
@@ -16,7 +16,7 @@ use app\components\AppUtility;
         </li>
         <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#"><?php AppUtility::t('Enroll Students');?><span class="caret"></span></a>
-            <ul class="dropdown-menu enroll-options roster-enroll-options">
+            <ul class="dropdown-menu enroll-options roster-enroll-options min-with-hundred-per">
                 <li><a href="<?php echo AppUtility::getURLFromHome('roster/roster', 'student-enrollment?cid='.$course->id.'&enroll=student'); ?>"><?php AppUtility::t('With known username');?></a></li>
                 <li><a href="<?php echo AppUtility::getURLFromHome('roster/roster', 'enroll-from-other-course?cid='.$course->id); ?>"><?php AppUtility::t('From another Course');?></a></li>
                 <li><a href="<?php echo AppUtility::getURLFromHome('roster/roster', 'create-and-enroll-new-student?cid='.$course->id); ?>"><?php AppUtility::t('Create new student');?></a></li>
