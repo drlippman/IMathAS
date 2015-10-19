eb2b134daf3560ce7a14d27669817e3d0b9dad1e
Abhishek Prajapati <abhishek.prajapati@tudip.com>
2015-10-19 12:47:59 +0530

    Small Fixes: Changed the revision number constant file.

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -14,21 +14,21 @@ class AppAsset extends AssetBundle
         'yii\web\YiiAsset',
     ];
     public $css = [
-        'css/imascore.css?ver=18',
-        'css/jquery-ui.min.css?ver=18',
-        'css/jquery-ui.structure.min.css?ver=18',
-        'css/site.css?ver=18',
-        'css/default.css?ver=18',
-        'css/font-awesome.min.css?ver=18'
+        'css/imascore.css?ver=19',
+        'css/jquery-ui.min.css?ver=19',
+        'css/jquery-ui.structure.min.css?ver=19',
+        'css/site.css?ver=19',
+        'css/default.css?ver=19',
+        'css/font-awesome.min.css?ver=19'
     ];
     public $jsOptions = array(
         'position' => \yii\web\View::POS_HEAD
     );
     public $js = [
         'js/mathjax/MathJax.js?config=AM_HTMLorMML',
-        'js/ASCIIsvg_min.js?ver=18',
-        'js/mathgraphcheck.js?ver=18',
-        'js/jquery-ui.min.js?ver=18',
-        'js/common.js?ver=18',
+        'js/ASCIIsvg_min.js?ver=19',
+        'js/mathgraphcheck.js?ver=19',
+        'js/jquery-ui.min.js?ver=19',
+        'js/common.js?ver=19',
     ];
 }
