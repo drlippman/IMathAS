45f130d7f7023133ce872b8bbfd85d222e260d86
amolkhaire <amol.khaire@tudip.nl>
2015-11-23 11:56:15 +0530

    Feature #24796: Mobile supportive UI: Gradebook: View Assessment Details.  -Applied the media query for the 'View Assessment Details' page.  -Written conditions in media query to apply the different styling on different mobile devices.  -Handled the condition to show page content on different mobile devices in portrait and landscape mode.  -Applied the proper styling and changed the html code of the following elements.  -Make Exception button.  -Clear attempt and clear score link.  -View as student and print version link.  -Record Changed Grades button.  -Show Last Attempts and Show Review Attempts links.  -Hide and show question button.  -Preview all button.  -All question text-box.  -Feedback text-area.  -Record Changed Grades button. Feature #24797: Mobile supportive UI: Gradebook: exception.  -Applied the media query for the 'exception' page.  -Written conditions in media query to apply the different styling on different mobile devices.  -Handled the condition to show page content on different mobile devices in portrait and landscape mode.  -Applied the proper styling and changed the html code of the following elements.  -Assessment drop-down.  -Available Until date and time picker.  -Available After date and time picker.  -Create exception button.  -All check-boxes.

--- views/gradebook/gradebook/exception.php
+++ views/gradebook/gradebook/exception.php
@@ -18,22 +18,29 @@ if ($overwriteBody == 1) {
 </script>
 <div class=breadcrumb><?php echo $curBreadcrumb ?></div>
 <div id="headerexception" class="pagetitle"><h2>Make Start/Due Date Exception</h2></div>
-<div class="tab-content shadowBox">
+<div class="tab-content shadowBox col-md-12 col-sm-12 padding-bottom-two-em">
     <?php
-    echo '<h3>' . $stuname . '</h3>';
+    echo '<div class="col-md-12"><h3 class="col-md-12 col-sm-12">' . $stuname . '</h3></div>';
     echo $page_isExceptionMsg;
-    echo '<p><span class="form">Assessment:</span><span class="formright">';
-    AssessmentUtility::writeHtmlSelect("aidselect", $page_courseSelect['val'], $page_courseSelect['label'], $params['aid'], "Select an assessment", "", " onchange='nextpage()'");
-    echo '</span><br class="form"/></p>';
+    echo '
+    <div class="col-md-12 col-sm-12 padding-top-one-pt-five-em">
+        <span class="col-md-2 col-sm-2">Assessment</span>
+        <span class="padding-left-zero col-md-3 col-sm-4 padding-left-two-pt-three-em">';
+        AssessmentUtility::writeHtmlSelect("aidselect", $page_courseSelect['val'], $page_courseSelect['label'], $params['aid'], "Select an assessment", "", " onchange='nextpage()'");
+        echo '
+        </span>
+    </div>';
 
     if (isset($params['aid']) && $params['aid'] != '') {
         ?>
         <form method=post
               action="exception?cid=<?php echo $course->id ?>&aid=<?php echo $params['aid'] ?>&uid=<?php echo $params['uid'] ?>&asid=<?php echo $asid; ?>&from=<?php echo $from; ?>">
-            <div class="col-md-12 padding-left-zero">
-                <div class="col-md-5 padding-left-zero" id="datePicker-id1">
-                    <span class="select-text-margin col-md-4"><?php AppUtility::t('Available After') ?></span>
-                                <span class="col-md-8">
+            <div class="col-md-12 col-sm-12 padding-top-one-pt-five-em">
+                <span class="select-text-margin col-md-2 col-sm-2 padding-right-zero">
+                    <?php AppUtility::t('Available After') ?>
+                </span>
+                <div class="col-md-3 col-sm-4 padding-left-zero" id="datePicker-id1">
+                                <span class="padding-left-zero col-md-12 col-sm-12">
                                     <?php
                                     echo DatePicker::widget([
                                         'name' => 'sdate',
@@ -46,10 +53,12 @@ if ($overwriteBody == 1) {
                                     ]);
                                     ?>
                                 </span>
+
                 </div>
-                <div class="col-md-5 padding-left-zero" id="timepicker-id">
-                    <span class="select-text-margin col-md-1 padding-left-five"><?php AppUtility::t('at') ?></span>
-                                <span class="col-md-11">
+                <span class="select-text-margin floatleft"><?php AppUtility::t('at') ?></span>
+                <div class="col-md-4 col-sm-5" id="timepicker-id">
+
+                                <span class="col-md-12 col-sm-12">
                                 <?php
                                 echo TimePicker::widget([
                                     'name' => 'stime',
@@ -66,12 +75,10 @@ if ($overwriteBody == 1) {
                 </div>
             </div>
 
-            <br><br><br>
-
-            <div class="col-md-12 padding-left-zero">
-                <div class="col-md-5 padding-left-zero" id="datePicker-id1">
-                    <span class="select-text-margin col-md-4"><?php AppUtility::t('Available After') ?></span>
-                                <span class="col-md-8">
+            <div class="col-md-12 col-sm-12 padding-top-one-pt-five-em">
+                <span class="select-text-margin col-md-2 col-sm-2 padding-right-zero"><?php AppUtility::t('Available After') ?></span>
+                <div class="col-md-3 col-sm-4 padding-left-zero" id="datePicker-id1">
+                                <span class="col-md-12 col-sm-12 padding-left-zero">
                                     <?php
                                     echo DatePicker::widget([
                                         'name' => 'edate',
@@ -85,9 +92,10 @@ if ($overwriteBody == 1) {
                                     ?>
                                 </span>
                 </div>
-                <div class="col-md-5 padding-left-zero" id="timepicker-id">
-                    <span class="select-text-margin col-md-1 padding-left-five"><?php AppUtility::t('at') ?></span>
-                                <span class="col-md-11">
+                <span class="select-text-margin floatleft"><?php AppUtility::t('at') ?></span>
+                <div class="col-md-4 col-sm-5" id="timepicker-id">
+
+                                <span class="col-md-12 col-sm-12">
                                 <?php
                                 echo TimePicker::widget([
                                     'name' => 'etime',
@@ -103,22 +111,40 @@ if ($overwriteBody == 1) {
                                  </span>
                 </div>
             </div>
-            <span class="form"><input type="checkbox" name="forceregen"/></span>
-		<span class="formright">Force student to work on new versions of all zquestions?  Students 
-		   will keep any scores earned, but must work new versions of questions to improve score.</span><br
-                class="form"/>
-            <span class="form"><input type="checkbox" name="eatlatepass"/></span>
-		<span class="formright">Deduct <input type="input" name="latepassn" size="1" value="1"/> LatePass(es).  
-		   Student currently has <?php echo $latepasses; ?> latepasses.</span><br class="form"/>
-            <span class="form"><input type="checkbox" name="waivereqscore"/></span>
-            <span class="formright">Waive "show based on an another assessment" requirements, if applicable.</span><br
-                class="form"/>
 
-            <div class=submit><input type=submit value="<?php echo $savetitle; ?>"></div>
+            <div class="col-md-offset-2 col-sm-offset-2 col-md-10 col-sm-10 padding-top-one-pt-five-em padding-left-ten">
+                <span class="floatleft">
+                    <input type="checkbox" name="forceregen"/>
+                </span>
+                <span class="col-md-11 col-sm-11">
+                    Force student to work on new versions of all zquestions?  Students
+                    will keep any scores earned, but must work new versions of questions to improve score.
+                </span>
+            </div>
+
+            <div class="col-md-offset-2 col-sm-offset-2 col-md-10 col-sm-10 padding-top-one-pt-five-em padding-left-ten">
+                <span class="floatleft padding-top-five">
+                    <input type="checkbox" name="eatlatepass"/>
+                </span>
+                 <span class="col-md-11 col-sm-11">Deduct
+                    <input class="form-control width-ten-per display-inline-block" type="input" name="latepassn" size="1" value="1"/> LatePass(es).
+                    Student currently has <?php echo $latepasses; ?> latepasses.
+                </span>
+            </div>
+
+            <div class="col-md-offset-2 col-sm-offset-2 col-md-10 col-sm-10 padding-top-one-pt-five-em padding-left-ten">
+                <span class="floatleft">
+                    <input type="checkbox" name="waivereqscore"/>
+                </span>
+                 <span class="col-md-11 col-sm-11">
+                     Waive "show based on an another assessment" requirements, if applicable.
+                </span>
+            </div>
+
+            <div class="col-md-offset-2 col-sm-offset-2 col-md-4 col-sm-4 padding-top-one-pt-five-em padding-left-ten">
+                <input type=submit value="<?php echo $savetitle; ?>">
+            </div>
         </form>
-    <?php
-    }
-    echo '</div>';
-    }
-
-    ?>
+    <?php } ?>
+   </div>
+<?php } ?>
