95a8a18ae69189d8c40b1a5669203df75fb7d9c3
Nilkesh Surana <nilkesh.surana@tudip.nl>
2016-02-18 17:41:09 +0530

    Bug #27838 : Modify Wiki not working.     -Fixed the functionality of adding the description to the existing Wiki.     -Fixed the functionality of updating the existing description of wiki.     -Fixed the double loading of call of Editor.js     -Applied fixes to the default description loading and retrieving description from database.

--- controllers/wiki/WikiController.php
+++ controllers/wiki/WikiController.php
@@ -610,7 +610,7 @@ class WikiController extends AppController
                 return $this->redirect(AppUtility::getURLFromHome('course', 'course/course?cid=' .$courseId));
             }
         }
-       $this->includeJS(["course/inlineText.js","editor/tiny_mce.js" , 'editor/tiny_mce_src.js', 'general.js', 'editor.js']);
+       $this->includeJS(["course/inlineText.js","editor/tiny_mce.js" , 'editor/tiny_mce_src.js', 'general.js']);
         $this->includeCSS(["roster/roster.css", 'course/items.css']);
         $returnData = array('course' => $course, 'saveTitle' => $saveTitle, 'wiki' => $wiki, 'groupNames' => $groupNames, 'defaultValue' => $defaultValues, 'page_formActionTag' => $page_formActionTag, 'revisedate' => $revisedate, 'rdate' => $rdate, 'rtime' => $rtime,'started' => $started,
                 'page_groupSelect'=>$page_groupSelect);
