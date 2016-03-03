88503ae309d648da73b704f6336da38b633d6556
Nilkesh Surana <nilkesh.surana@tudip.nl>
2016-03-03 15:20:28 +0530

    Bug #28352: Found exception on click of roster breadcrumb.  -Fixed the redirection of breadcrumbs of Roster's login log page.  -Fixed the redirection of course name breadcrumbs link to the course page in case of roster view.  -Updated the link path to redirect to course page.  -Fixed the redirection of course name breadcrumbs link to the course page in case of gradebook view.  -Updated the link path to redirect to course page.  -Updated the link path with required parameters.

--- views/roster/roster/loginLog.php
+++ views/roster/roster/loginLog.php
@@ -8,11 +8,11 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="item-detail-header">
     <?php if(isset($from) && $from=='gb'){
         echo $this->render("../../itemHeader/_indexWithLeftContent", ['link_title' => [AppUtility::t('Home', false), $course->name, AppUtility::t('Gradebook', false),AppUtility::t('Student Detail', false)],
-            'link_url' => [AppUtility::getHomeURL() . 'site/index', AppUtility::getHomeURL() . 'course/course/course?cid=' . $course->id,
+            'link_url' => [AppUtility::getHomeURL() . 'site/index',AppUtility::getURLFromHome('course','course/course?cid='.$course->id),
                 AppUtility::getURLFromHome('gradebook','gradebook/grade-book-student-detail?cid='.$course->id.'&studentId=0'),AppUtility::getURLFromHome('gradebook','gradebook/grade-book-student-detail?cid='.$course->id.'&studentId='.$userId)]]);
     }else{
         echo $this->render("../../itemHeader/_indexWithLeftContent", ['link_title' => [AppUtility::t('Home', false), $course->name, AppUtility::t('Roster', false)],
-            'link_url' => [AppUtility::getHomeURL() . 'site/index', AppUtility::getHomeURL() . 'course', 'course/course?cid=' . $course->id, AppUtility::getURLFromHome('roster','roster/student-roster?cid='.$course->id)]]);
+            'link_url' => [AppUtility::getHomeURL() . 'site/index',AppUtility::getURLFromHome('course','course/course?cid='.$course->id), AppUtility::getURLFromHome('roster','roster/student-roster?cid='.$course->id)]]);
 
     } ?>
 </div>
