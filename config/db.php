9f036e310f88a2533bc738d22606b68cdf178fd0
Bracken Mosbacker <bracken@lumenlearning.com>
2016-04-14 10:14:54 -0700

    set db config

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=imathasdb',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
