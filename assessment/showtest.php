7b02b7a9a9e4350d29276d6c6878232088ceca06
drlippman <drlippman@yahoo.com>
2016-07-11 00:21:27 -0700

    Add'l drawing merge tweaks

--- assessment/showtest.php
+++ assessment/showtest.php
@@ -940,7 +940,7 @@ if (!isset($_REQUEST['embedpostback'])) {
 	}
 	if ($testsettings['displaymethod'] == "LivePoll") {
 		$placeinhead .= '<script src="https://'.$CFG['GEN']['livepollserver'].':3000/socket.io/socket.io.js"></script>';
-		$placeinhead .= '<script src="'.$imasroot.'/javascript/livepoll.js"></script>';
+		$placeinhead .= '<script src="'.$imasroot.'/javascript/livepoll.js?v=071116"></script>';
 		$livepollroom = $testsettings['id'].'-'.($sessiondata['isteacher']?'teachers':'students');
 		$now = time();
 		if (isset($CFG['GEN']['livepollpassword'])) {
