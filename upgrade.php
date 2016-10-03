495898dae252e99fd1cc2a5abdf721558b9a2b18
drlippman <drlippman@yahoo.com>
2016-10-03 15:29:57 -0700

    Bug fix rand in mathphp needs to use RND

--- upgrade.php
+++ upgrade.php
@@ -5,8 +5,8 @@ $latest = 113;
 
 
 @set_time_limit(0);
-ini_set("max_input_time", "600");
-ini_set("max_execution_time", "600");
+ini_set("max_input_time", "6000");
+ini_set("max_execution_time", "6000");
 ini_set("memory_limit", "104857600");
 
 if (!empty($dbsetup)) {  //initial setup - just write upgradecounter.txt
