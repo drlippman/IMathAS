3a3a72b13eed4738a3e50067a2b248c18275486a
drlippman <drlippman@yahoo.com>
2016-11-06 17:43:08 -0800

    Add FCM serviceworker

---
+++ firebase-messaging-sw.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/javascript');
+require("config.php");
+?>
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/3.5.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.3/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  'messagingSenderId': '<?php echo $CFG['FCM']['SenderId']; ?>'
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
