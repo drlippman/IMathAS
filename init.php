a59cdf682cec743c07056d49e9f93d1e376aab6b
Michael Ackerman <michael@lumenlearning.com>
2017-06-08 15:40:01 -0700

    Enabled storage of PHP sessions in the DB. Enabled OWASP CSRF protector.

--- init.php
+++ init.php
@@ -14,12 +14,12 @@ if (!file_exists(__DIR__ . "/config.php")) {
 require_once(__DIR__ . "/config.php");
 
 // Store PHP sessions in the database.
-//require_once(__DIR__ . "/includes/session.php");
-//session_set_save_handler(new SessionDBHandler(), true);
+require_once(__DIR__ . "/includes/session.php");
+session_set_save_handler(new SessionDBHandler(), true);
 
 // OWASP CSRF Protector
-//require_once(__DIR__ . "/csrfp/libs/csrf/csrfprotector.php");
-//csrfProtector::init();
+require_once(__DIR__ . "/csrfp/libs/csrf/csrfprotector.php");
+csrfProtector::init();
 
 // Load validate.php?
 if (!isset($init_skip_validate) || (isset($init_skip_validate) && false == $init_skip_validate)) {
