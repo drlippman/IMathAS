084f2381532be588117cf932da9754fdc0aa0911
David Lippman <drlippman@yahoo.com>
2017-07-05 13:15:15 -0700

    Add groupsetid index to imas_assessments

---
+++ migrations/128_add_indices_2.php
@@ -0,0 +1,20 @@
+<?php
+//Need to revamp managelibs, manageqset DELETE again.
+
+//Add Federation tables
+$DBH->beginTransaction();
+
+
+ $query = "ALTER TABLE  `imas_assessments` ADD INDEX ( `groupsetid` )";
+ $res = $DBH->query($query);
+ if ($res===false) {
+ 	 echo "<p>Query failed: ($query) : " . $DBH->errorInfo() . "</p>";
+	$DBH->rollBack();
+	return false;
+ }
+
+$DBH->commit();
+
+echo "<p style='color: green;'>✓ Added indices</p>";
+
+return true;
