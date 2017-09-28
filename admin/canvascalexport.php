77554ac06ff78605883444cdf9dedd1032ccaf45
drlippman <drlippman@yahoo.com>
2017-09-27 18:38:38 -0700

    Add stuonly option for canvas cal export Ensure resource ID stays the same

--- admin/canvascalexport.php
+++ admin/canvascalexport.php
@@ -9,22 +9,20 @@ if (!isset($teacherid)) {
 	echo "You are not authorized to view this page";
 	exit;
 }
-list($coursename,$calevents) = getCalendarEventData($cid, $userid);
-
-$uniqid = uniqid();
+list($coursename,$calevents) = getCalendarEventData($cid, $userid, isset($_GET['stuonly']));
 
 $zip = new ZipArchive();
 $file = tempnam(__DIR__.'/import', "zip");
 $zip->open($file, ZipArchive::OVERWRITE);
 
 $manifest = '<?xml version="1.0" encoding="UTF-8"?>
-<manifest identifier="'.$uniqid.'" xmlns="http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1" xmlns:lom="http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource" xmlns:lomimscc="http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1 http://www.imsglobal.org/profile/cc/ccv1p1/ccv1p1_imscp_v1p2_v1p0.xsd http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource http://www.imsglobal.org/profile/cc/ccv1p1/LOM/ccv1p1_lomresource_v1p0.xsd http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest http://www.imsglobal.org/profile/cc/ccv1p1/LOM/ccv1p1_lommanifest_v1p0.xsd">
+<manifest identifier="'.Sanitize::simpleString($installname.'-calevents-'.$cid).'" xmlns="http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1" xmlns:lom="http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource" xmlns:lomimscc="http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.imsglobal.org/xsd/imsccv1p1/imscp_v1p1 http://www.imsglobal.org/profile/cc/ccv1p1/ccv1p1_imscp_v1p2_v1p0.xsd http://ltsc.ieee.org/xsd/imsccv1p1/LOM/resource http://www.imsglobal.org/profile/cc/ccv1p1/LOM/ccv1p1_lomresource_v1p0.xsd http://ltsc.ieee.org/xsd/imsccv1p1/LOM/manifest http://www.imsglobal.org/profile/cc/ccv1p1/LOM/ccv1p1_lommanifest_v1p0.xsd">
 	<metadata>
     <schema>IMS Common Cartridge</schema>
     <schemaversion>1.1.0</schemaversion>
 	</metadata>
 	<resources>
-		<resource identifier="'.$uniqid.'res" type="associatedcontent/imscc_xmlv1p1/learning-application-resource" href="course_settings/canvas_export.txt">
+		<resource identifier="'.Sanitize::simpleString($installname.'-caleventsres-'.$cid).'res" type="associatedcontent/imscc_xmlv1p1/learning-application-resource" href="course_settings/canvas_export.txt">
 			<file href="course_settings/events.xml"/>
 		</resource>
 	</resources>
@@ -34,9 +32,10 @@ $zip->addFromString("imsmanifest.xml", $manifest);
 $zip->addFromString("course_settings/canvas_export.txt",'Q: What did the panda say when he was forced out of his natural habitat? A: This is un-BEAR-able');
 
 $events = '<?xml version="1.0" encoding="UTF-8"?>
-<events xmlns="http://canvas.instructure.com/xsd/cccv1p0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://canvas.instructure.com/xsd/cccv1p0 http://canvas.instructure.com/xsd/cccv1p0.xsd">';
+<events xmlns="http://canvas.instructure.com/xsd/cccv1p0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://canvas.instructure.com/xsd/cccv1p0 http://canvas.instructure.com/xsd/cccv1p0.xsd">
+';
 foreach ($calevents as $k=>$event) {
-	$events .= '<event identifier="'.$uniqid.'e'.$k.'">'."\n";
+	$events .= '<event identifier="'.Sanitize::simpleString($installname.'-calevent-'.$event[0]).'">'."\n";
 	$events .= '<title>' . htmlentities($event[2],ENT_XML1,'UTF-8',false) . '</title>'."\n";
 	if ($event[3]!='') {
 		$event[3] = str_replace(_('Assignment Due: '), sprintf(_('Assignment Due in %s: '), $installname), $event[3]);
--- includes/calendardata.php
+++ includes/calendardata.php
@@ -2,9 +2,9 @@
 
 //light calendar data collection function
 
-function getCalendarEventData($cid, $userid) {
+function getCalendarEventData($cid, $userid, $stuview = false) {
 	global $DBH;
-	
+
 	$studentinfo = array();
 	$stm = $DBH->prepare("SELECT id,section FROM imas_students WHERE userid=:userid AND courseid=:courseid");
 	$stm->execute(array(':userid'=>$userid, ':courseid'=>$cid));
@@ -18,11 +18,7 @@ function getCalendarEventData($cid, $userid) {
 		$stm->execute(array(':userid'=>$userid, ':courseid'=>$cid));
 		$line = $stm->fetch(PDO::FETCH_ASSOC);
 		if ($line != null) {
-			if ($myrights>19) {
-				$teacherid = $line['id'];
-			} else {
-				$tutorid = $line['id'];
-			}
+			$teacherid = $line['id'];
 		} else {
 			$stm = $DBH->prepare("SELECT id,section FROM imas_tutors WHERE userid=:userid AND courseid=:courseid");
 			$stm->execute(array(':userid'=>$userid, ':courseid'=>$cid));
@@ -33,14 +29,17 @@ function getCalendarEventData($cid, $userid) {
 			}
 		}
 	}
-	
+	if ($stuview) {
+		$studentinfo['timelimitmult'] = 1;
+		$studentinfo['section'] = "not a real section name";
+	}
 	if (!isset($teacherid) && !isset($tutorid) && !isset($studentid)) {
 		echo 'Invalid token: invalid user credentials';
 		exit;
 	}
-				
+
 	$now= time();
-	
+
 	$exceptions = array();
 	$forumexceptions = array();
 	if (isset($studentid)) {
@@ -60,17 +59,17 @@ function getCalendarEventData($cid, $userid) {
 	while ($r = $stm2->fetch(PDO::FETCH_NUM)) {
 		$viewedassess[] = $r[0];
 	}
-	
-	
+
+
 	$stm = $DBH->prepare("SELECT name,itemorder FROM imas_courses WHERE id=:id");
 	$stm->execute(array(':id'=>$cid));
 	$row = $stm->fetch(PDO::FETCH_NUM);
 	$coursename = trim($row[0]);
 	$itemorder = unserialize($row[1]);
 	$itemsimporder = array();
-	
-	flattenitems($itemorder,$itemsimporder,(isset($teacherid)||isset($tutorid)), $studentinfo);
-	
+
+	flattenitems($itemorder,$itemsimporder,(isset($teacherid)||isset($tutorid))&&!$stuview, $studentinfo);
+
 	$tolookup = implode(',', array_map('intval', $itemsimporder));
 	$itemlist = array();
 	if (count($itemsimporder)>0) {
@@ -82,29 +81,29 @@ function getCalendarEventData($cid, $userid) {
 			$itemlist[$row[1]][$row[2]] = $row[0];
 		}
 	}
-	
-	
+
+
 	$calevents = array();
-	
+
 	$bestscores_stm = null;
-	
+
 	if (isset($itemlist['Assessment'])) {
 		$typeids = implode(',', array_keys($itemlist['Assessment']));
 		$stm = $DBH->query("SELECT id,name,startdate,enddate,reviewdate,reqscore,reqscoreaid FROM imas_assessments WHERE avail=1 AND id IN ($typeids) AND enddate<2000000000 ORDER BY name");
 		while ($row = $stm->fetch(PDO::FETCH_ASSOC)) {
 			require_once("../includes/exceptionfuncs.php");
 			if (isset($exceptions[$row['id']])) {
-				$useexception = getCanUseAssessException($exceptions[$row['id']], $row, true); 
+				$useexception = getCanUseAssessException($exceptions[$row['id']], $row, true);
 				if ($useexception) {
 					$row['startdate'] = $exceptions[$row['id']][0];
 					$row['enddate'] = $exceptions[$row['id']][1];
 				}
-			} 
+			}
 			//if startdate is past now, skip
-			if ($row['startdate']>$now && !isset($teacherid)) {
+			if ($row['startdate']>$now && (!isset($teacherid) || $stuview)) {
 				continue;
 			}
-			
+
 			$showgrayedout = false;
 			if (!isset($teacherid) && abs($row['reqscore'])>0 && $row['reqscoreaid']>0 && (!isset($exceptions[$row['id']]) || $exceptions[$row['id']][3]==0)) {
 				if ($bestscores_stm===null) { //only prepare once
@@ -127,6 +126,12 @@ function getCalendarEventData($cid, $userid) {
 						}
 					}
 				}
+			} else if ($stuview && abs($row['reqscore'])>0 && $row['reqscoreaid']>0) {
+				if ($row['reqscore']<0) {
+					$showgrayedout = true;
+				} else {
+					continue;
+				}
 			}
 			if ($row['reviewdate']>0 && $row['reviewdate']<2000000000 && $now>$row['enddate']) { //has review, and we're past enddate
 				//do we care about review dates?
@@ -136,17 +141,17 @@ function getCalendarEventData($cid, $userid) {
 			}
 		}
 	}
-	
+
 	if (isset($itemlist['InlineText'])) {
 		$typeids = implode(',', array_keys($itemlist['InlineText']));
-		if (isset($teacherid)) {
+		if (isset($teacherid) && !$stuview) {
 			$stm = $DBH->prepare("SELECT id,title,enddate,startdate,oncal,text FROM imas_inlinetext WHERE ((oncal=2 AND enddate>0 AND enddate<2000000000) OR (oncal=1 AND startdate<2000000000 AND startdate>0)) AND (avail=1 OR (avail=2 AND startdate>0)) AND id IN ($typeids)");
 			$stm->execute(array(':courseid'=>$cid));
 		} else {
 			$query = "SELECT id,title,enddate,text,startdate,oncal,caltag,avail,text FROM imas_inlinetext WHERE ";
 			$query .= "((avail=1 AND ((oncal=2 AND enddate>0 AND enddate<2000000000 AND startdate<$now) OR (oncal=1 AND startdate<$now AND startdate>0))) OR ";
 			$query .= "(avail=2 AND oncal=1 AND startdate<2000000000 AND startdate>0)) AND id IN ($typeids)";
-			$stm = $DBH->query($query); 
+			$stm = $DBH->query($query);
 		}
 		while ($row = $stm->fetch(PDO::FETCH_ASSOC)) {
 			if ($row['title']=='##hidden##') {
@@ -163,17 +168,17 @@ function getCalendarEventData($cid, $userid) {
 			$calevents[] = array('I'.$row['id'],$date, $row['title'],'');
 		}
 	}
-	
+
 	if (isset($itemlist['LinkedText'])) {
 		$typeids = implode(',', array_keys($itemlist['LinkedText']));
-		if (isset($teacherid)) {
+		if (isset($teacherid) && !$stuview) {
 			$query = "SELECT id,title,enddate,startdate,oncal FROM imas_linkedtext WHERE ((oncal=2 AND enddate>0 AND enddate<2000000000) OR (oncal=1 AND startdate<2000000000 AND startdate>0)) AND (avail=1 OR (avail=2 AND startdate>0)) AND id IN ($typeids) ORDER BY title";
 		} else {
 			$query = "SELECT id,title,enddate,startdate,oncal FROM imas_linkedtext WHERE ";
 			$query .= "((avail=1 AND ((oncal=2 AND enddate>0 AND enddate<2000000000 AND startdate<$now) OR (oncal=1 AND startdate<$now AND startdate>0))) OR ";
 			$query .= "(avail=2 AND oncal=1 AND startdate<2000000000 AND startdate>0)) AND id IN ($typeids) ORDER BY title";
 		}
-		$stm = $DBH->query($query); 
+		$stm = $DBH->query($query);
 		while ($row = $stm->fetch(PDO::FETCH_ASSOC)) {
 			if ($row['oncal']==1) {
 				$date = $row['startdate'];
@@ -183,33 +188,33 @@ function getCalendarEventData($cid, $userid) {
 			$calevents[] = array('L'.$row['id'],$date, $row['title'],'');
 		}
 	}
-	
+
 	if (isset($itemlist['Drill'])) {
 		$typeids = implode(',', array_keys($itemlist['Drill']));
-		if (isset($teacherid)) {
+		if (isset($teacherid) && !$stuview) {
 			$query = "SELECT id,name,enddate,startdate,caltag,avail FROM imas_drillassess WHERE (enddate>0 AND enddate<2000000000) AND avail=1 AND id IN ($typeids) ORDER BY name";
 		} else {
 			$query = "SELECT id ,name,enddate,startdate,caltag,avail FROM imas_drillassess WHERE ";
 			$query .= "avail=1 AND (enddate>0 AND enddate<2000000000 AND startdate<$now) ";
 			$query .= "AND id IN ($typeids) ORDER BY name";
 		}
-		$stm = $DBH->query($query); 
+		$stm = $DBH->query($query);
 		while ($row = $stm->fetch(PDO::FETCH_ASSOC)) {
 			$calevents[] = array('D'.$row['id'],$row['enddate'], $row['name'], _('Drill Due: ').$row['name']);
-		}	
+		}
 	}
-	
+
 	if (isset($itemlist['Forum'])) {
 		$typeids = implode(',', array_keys($itemlist['Forum']));
 		$query = "SELECT id,name,postby,replyby,startdate,enddate FROM imas_forums WHERE enddate>0 AND ((postby>0 AND postby<2000000000) OR (replyby>0 AND replyby<2000000000)) AND avail>0 AND id IN ($typeids) ORDER BY name";
-		$stm = $DBH->query($query); 
+		$stm = $DBH->query($query);
 		while ($row = $stm->fetch(PDO::FETCH_ASSOC)) {
-			if (($row['startdate']>$now && !isset($teacherid))) {
+			if ($row['startdate']>$now && (!isset($teacherid) || $stuview)) {
 				continue;
 			}
 			require_once("../includes/exceptionfuncs.php");
 			list($canundolatepassP, $canundolatepassR, $canundolatepass, $canuselatepassP, $canuselatepassR, $row['postby'], $row['replyby'], $row['enddate']) = getCanUseLatePassForums(isset($forumexceptions[$row['id']])?$forumexceptions[$row['id']]:null, $row);
-			
+
 			if ($row['postby']!=2000000000) {
 				$calevents[] = array('FP'.$row['id'],$row['postby'], $row['name'], _('Posts Due: ').$row['name']);
 			}
@@ -218,13 +223,13 @@ function getCalendarEventData($cid, $userid) {
 			}
 		}
 	}
-	
+
 	$stm = $DBH->prepare("SELECT title,tag,date,id FROM imas_calitems WHERE date>0 AND date<2000000000 and courseid=:courseid ORDER BY title");
 	$stm->execute(array(':courseid'=>$cid));
 	while ($row = $stm->fetch(PDO::FETCH_ASSOC)) {
 		$calevents[] = array('C'.$row['id'],$row['date'], $row['tag'], $row['title']);
 	}
-	
+
 	return array($coursename,$calevents);
 }
 
