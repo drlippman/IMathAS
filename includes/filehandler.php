f0fc285eab76dd11c24b96ab39fddc9f64e54466
drlippman <drlippman@yahoo.com>
2018-04-05 17:22:43 -0700

    Add memory use check to downsizeimage
    Also, consolidate orientation correction with downsize

--- includes/filehandler.php
+++ includes/filehandler.php
@@ -148,7 +148,6 @@ function storeuploadedfile($id,$key,$sec="private") {
 			$sec = "private";
 		}
 		if (is_uploaded_file($_FILES[$id]['tmp_name'])) {
-			correctimageorientation($_FILES[$id]);
 			downsizeimage($_FILES[$id]);
 			$s3 = new S3($GLOBALS['AWSkey'],$GLOBALS['AWSsecret']);
 			// $_FILES[]['tmp_name'] is not user provided. This is safe.
@@ -162,7 +161,6 @@ function storeuploadedfile($id,$key,$sec="private") {
 		}
 	} else {
 		if (is_uploaded_file($_FILES[$id]['tmp_name'])) {
-			correctimageorientation($_FILES[$id]);
 			downsizeimage($_FILES[$id]);
 			$base = rtrim(dirname(dirname(__FILE__)), '/\\').'/filestore/';
 			$key = Sanitize::sanitizeFilePathAndCheckBlacklist($key);
@@ -182,45 +180,48 @@ function storeuploadedfile($id,$key,$sec="private") {
 	}
 }
 
-function correctimageorientation($fileinfo) {
-	if (preg_match('/\.(jpg|jpeg)/', $fileinfo['name'])) {
-		$imgdata = getimagesize($fileinfo['tmp_name']);
-		if ($imgdata!==false && $imgdata['mime'] == 'image/jpeg') {
-			$exif = exif_read_data($fileinfo['tmp_name']);
-			if (isset($exif['Orientation']) && $exif['Orientation']>1) {
-				$image = imagecreatefromjpeg($fileinfo['tmp_name']);
-				switch($exif['Orientation']) {
-					case 3:
-					    $image = imagerotate($image, 180, 0);
-					    break;
-					case 6:
-					    $image = imagerotate($image, -90, 0);
-					    break;
-					case 8:
-					    $image = imagerotate($image, 90, 0);
-					    break;
-				}
-				imagejpeg($image, $fileinfo['tmp_name'], 90);
-			}
-		}
-	}
-}
 function downsizeimage($fileinfo) {
 	if (preg_match('/\.(jpg|jpeg)/', $fileinfo['name'])) {
 		$imgdata = getimagesize($fileinfo['tmp_name']);
-		if ($imgdata!==false && $imgdata['mime'] == 'image/jpeg' && min($imgdata[0],$imgdata[1])>1000) {
-			$r = $imgdata[0]/$imgdata[1]; // width/height
-			$image = imagecreatefromjpeg($fileinfo['tmp_name']);
-			if ($imgdata[0]>$imgdata[1]) { //width bigger than height
-				$newh = 1000;
-				$neww = round($r*$newh);
+		$exif = exif_read_data($fileinfo['tmp_name']);
+		$changed = false;
+		if ($imgdata!==false && $imgdata['mime'] == 'image/jpeg' && 
+		   (min($imgdata[0],$imgdata[1])>1000 || (isset($exif['Orientation']) && $exif['Orientation']>1)) &&
+		   ($imgdata[0]*$imgdata[1]*3*2/1048576 < 80)) {  //make sure mem use will be under 80MB
+			if (min($imgdata[0],$imgdata[1])>1000) {
+				$r = $imgdata[0]/$imgdata[1]; // width/height
+				$image = imagecreatefromjpeg($fileinfo['tmp_name']);
+				if ($imgdata[0]>$imgdata[1]) { //width bigger than height
+					$newh = 1000;
+					$neww = round($r*$newh);
+				} else {
+					$neww = 1000;
+					$newh = round($neww/$r);
+				}
+				$dst = imagecreatetruecolor($neww, $newh);
+				imagecopyresampled($dst, $image, 0, 0, 0, 0, $neww, $newh, $imgdata[0], $imgdata[1]);
+				imagedestroy($image);
 			} else {
-				$neww = 1000;
-				$newh = round($neww/$r);
+				$dst = imagecreatefromjpeg($fileinfo['tmp_name']);
 			}
-			$dst = imagecreatetruecolor($neww, $newh);
-			imagecopyresampled($dst, $image, 0, 0, 0, 0, $neww, $newh, $imgdata[0], $imgdata[1]);
+			if (isset($exif['Orientation']) && $exif['Orientation']>1) {
+				switch($exif['Orientation']) {
+					case 3:
+					    $dst = imagerotate($dst, 180, 0);
+					    $changed = true;
+					    break;
+					case 6:
+					    $dst = imagerotate($dst, -90, 0);
+					    $changed = true;
+					    break;
+					case 8:
+					    $dst = imagerotate($dst, 90, 0);
+					    $changed = true;
+					    break;
+				}
+			}
 			imagejpeg($dst, $fileinfo['tmp_name'], 90);
+			imagedestroy($dst);
 		}
 	}
 }
