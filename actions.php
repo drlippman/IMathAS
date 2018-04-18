88d2f45cf525d74984d2b479b16abe767d4899a7
Eric Rasmussen <erasmussen@eaglecrk.com>
2018-04-18 09:42:04 -0500

    updating actions.php

--- actions.php
+++ actions.php
@@ -37,6 +37,7 @@ require_once("includes/sanitize.php");
 		$_POST['email'] = Sanitize::emailAddress(trim($_POST['email']));
 		$_POST['firstname'] = Sanitize::stripHtmlTags(trim($_POST['firstname']));
 		$_POST['lastname'] = Sanitize::stripHtmlTags(trim($_POST['lastname']));
+		$_POST['courseid'] = Sanitize::courseId(trim($_POST['courseid']));
 
 		$error .= checkNewUserValidation();
 
@@ -69,7 +70,7 @@ require_once("includes/sanitize.php");
 			$homelayout = '|0,1,2||0,1';
 		}
 		if (isset($_POST['courseselect']) && $_POST['courseselect']>0) {
-			$_POST['courseid'] = $_POST['courseselect'];
+			$_POST['courseid'] = Sanitize::courseId(trim($_POST['courseselect']));
 			$_POST['ekey'] = '';
 		}
 		if (!isset($_GET['confirmed'])) {
@@ -229,7 +230,7 @@ require_once("includes/sanitize.php");
 
 		$query = "UPDATE imas_users SET rights=10 WHERE id=:id AND rights=0";
 		$stm = $DBH->prepare($query);
-		$stm->execute(array(':id'=>$_GET['id']));
+		$stm->execute(array(':id'=>Sanitize::onlyInt($_GET['id'])));
 
 		if ($stm->rowCount()>0) {
 			require("header.php");
@@ -411,7 +412,7 @@ require_once("includes/sanitize.php");
 		$stm->execute(array($sessionid));
 		$_SESSION = array();
 		if (isset($_COOKIE[session_name()])) {
-			setcookie(session_name(), '', time()-42000, '/');
+			setcookie(session_name(), '', time()-42000, '/', null, false, true);
 		}
 		session_destroy();
 	} else if ($_GET['action']=="chgpwd" || $_GET['action']=="forcechgpwd") {
@@ -570,7 +571,7 @@ require_once("includes/sanitize.php");
 		}
 	} else if ($_POST['action']=="unenroll") {
 		if ($myrights < 6) {
-			echo "<html><body>\nError: Guests can't unenroll from courses</body></html";
+			echo "<html><body>\nError: Guests can't unenroll from courses</body></html>";
 			exit;
 		}
 		if (!isset($_GET['cid'])) {
