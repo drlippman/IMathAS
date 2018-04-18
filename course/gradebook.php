5ea43b7904beb44a452925e8b6b95f95e731af74
Eric Rasmussen <erasmussen@eaglecrk.com>
2018-04-18 08:47:06 -0500

    add fix back in for gradebook

--- course/gradebook.php
+++ course/gradebook.php
@@ -63,7 +63,7 @@ if ($canviewall) {
 		$stm = $DBH->prepare("SELECT colorize FROM imas_gbscheme WHERE courseid=:courseid");
 		$stm->execute(array(':courseid'=>$cid));
 		$colorize = $stm->fetchColumn(0);
-		setcookie("colorize-$cid",$colorize);
+		setcookie("colorize-$cid",$colorize,0,'','',false,true);
 	}
 	if (isset($_GET['catfilter'])) {
 		$catfilter = $_GET['catfilter'];
