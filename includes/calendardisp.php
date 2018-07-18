3c7eda7134c38779de7df5b3801368ee0a04be4b
drlippman <drlippman@yahoo.com>
2018-07-18 10:20:23 -0700

    Show link to assessment in calendar after due date when in review mode

--- includes/calendardisp.php
+++ includes/calendardisp.php
@@ -648,7 +648,6 @@ foreach ($itemsimporder as $item) {
 				if ($byid['A'.$datetype.$itemsassoc[$item][1]][5]>0 && !isset($teacherid)) {  //hide link and grey if not current
 					$colors[$k] = '#ccc';
 					$assess[$moday][$k]['color'] = '#ccc';
-					$assess[$moday][$k]['inactive'] = true;
 				}
 				$k++;
 			}
