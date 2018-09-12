9261f80ba5c348e6cf9fe3e8b1bad16dfb7f7daa
David Lippman <drlippman@yahoo.com>
2018-09-11 19:33:51 -0700

    Expand first/lastname fields to 50 chars

---
+++ migrations/157_expand_namefields.php
@@ -0,0 +1,26 @@
+<?php
+
+//change 
+$DBH->beginTransaction();
+
+ $query = "ALTER TABLE `imas_users` MODIFY FirstName VARCHAR(50) NOT NULL";
+ $res = $DBH->query($query);
+ if ($res===false) {
+ 	 echo "<p>Query failed: ($query) : " . $DBH->errorInfo() . "</p>";
+	$DBH->rollBack();
+	return false;
+ }
+
+  $query = "ALTER TABLE `imas_users` MODIFY LastName VARCHAR(50) NOT NULL";
+ $res = $DBH->query($query);
+ if ($res===false) {
+ 	 echo "<p>Query failed: ($query) : " . $DBH->errorInfo() . "</p>";
+	$DBH->rollBack();
+	return false;
+ }
+ 
+$DBH->commit();
+
+echo "<p style='color: green;'>✓ Expanded First and Last Name fields to 50 chars</p>";
+
+return true;
