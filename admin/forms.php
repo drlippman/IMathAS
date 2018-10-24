drlippman/IMathAS

commit 98ff37829b55ad601c7bae4b6030c66f1b31e931

Add feature flag for showing LTI launch URL in course settings
$CFG['LTI']['showURLinSettings'] = true;

admin/forms.php
@@ -363,6 +363,11 @@ switch($_GET['action']) {
 					
 					echo '<span class="form">LTI Secret:</span>';
 					echo '<span class="formright">'.Sanitize::encodeStringForDisplay($line['ltisecret']).'</span><br class="form" />';
+					
+					if (!empty($CFG['LTI']['showURLinSettings'])) {
+						echo '<span class="form">LTI Launch URL (if needed):</span>';
+						echo '<span class="formright">'.Sanitize::encodeStringForDisplay($basesiteurl).'/bltilaunch.php</span><br class="form" />';
+					}
 					echo '</div>';
 				}
 				echo '</div>'; //end LTI grouping
@@ -717,7 +722,12 @@ switch($_GET['action']) {
 					echo '<span class="form">LTI Secret (max 10 chars)</span>';
 					echo '<span class="formright"><input name="ltisecret" type="text" value="'.Sanitize::encodeStringForDisplay($ltisecret).'" maxlength="10"/> ';
 					echo '</span><br class="form" />';
+					
 					echo '</div>';
+				}
+				if (!empty($CFG['LTI']['showURLinSettings'])) {
+					echo '<span class="form">LTI Launch URL (if needed):</span>';
+					echo '<span class="formright">'.Sanitize::encodeStringForDisplay($basesiteurl).'/bltilaunch.php</span><br class="form" />';
 				}
 			}
 			

commit 051a028acdf9906653f5a02f41fd3cab89f5bfbd

Bug fix: broken addcourse page
Fixes a regression from 98ff37829b55ad601c7bae4b6030c66f1b31e931

admin/forms.php
@@ -300,18 +300,20 @@ switch($_GET['action']) {
 		break;
 	case "modify":
 	case "addcourse":
-		$query = "SELECT ic.*,iu.FirstName,iu.LastName FROM imas_courses AS ic ";
-		$query .= "JOIN imas_users AS iu on ic.ownerid=iu.id WHERE ic.id=:id";
-		$stm = $DBH->prepare($query);
-		$stm->execute(array(':id'=>$_GET['id']));
-		if ($stm->rowCount()==0) {break;}
-		$line = $stm->fetch(PDO::FETCH_ASSOC);
-		
+		if ($_GET['action']=='modify') {
+			$query = "SELECT ic.*,iu.FirstName,iu.LastName FROM imas_courses AS ic ";
+			$query .= "JOIN imas_users AS iu on ic.ownerid=iu.id WHERE ic.id=:id";
+			$stm = $DBH->prepare($query);
+			$stm->execute(array(':id'=>$_GET['id']));
+			if ($stm->rowCount()==0) {break;}
+			$line = $stm->fetch(PDO::FETCH_ASSOC);
+		}
+
 		$stm = $DBH->prepare("SELECT id FROM imas_users WHERE (rights=11 OR rights=76 OR rights=77) AND groupid=?");
 		$stm->execute(array($groupid));
 		$hasGroupLTI = ($stm->fetchColumn() !== false);
-			
-		if (($myrights < 40 || ($line['ownerid']!=$userid && $myrights<75)) && $_GET['action']=='modify') {
+
+		if ($_GET['action']=='modify' && ($myrights < 40 || ($line['ownerid']!=$userid && $myrights<75))) {
 			//show limited info version
 			$stm = $DBH->prepare("SELECT id FROM imas_teachers WHERE courseid=? AND userid=?");
 			$stm->execute(array($_GET['id'], $userid));
@@ -374,6 +376,7 @@ switch($_GET['action']) {
 			}
 			break;
 		}
+		
 		if ($myrights < 40) { echo "You don't have the authority for this action"; break;}
 
 		$isadminview = false;