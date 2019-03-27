8d2e055934bb7cc94967d19e2777be23c0bf16eb
drlippman <drlippman@yahoo.com>
2019-03-27 12:14:07 -0700

    Clean up config files

--- assess2/vue-src/vue.config.js
+++ assess2/vue-src/vue.config.js
@@ -2,10 +2,6 @@ const path = require("path");
 
 module.exports = {
   outputDir: path.resolve(__dirname, '../vue'),
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/assess2/'
-    : '/',
-
   pluginOptions: {
     i18n: {
       locale: 'en',
@@ -14,17 +10,4 @@ module.exports = {
       enableInSFC: false
     }
   }
-  /* chainWebpack: config => {
-    config.module
-      .rule('i18n')
-      .resourceQuery(/blockType=i18n/)
-      .use('vue-i18n-loader2')
-        .loader('vue-i18n-loader2')
-        .options({
-          quite: false,
-          target: 'src/locales',
-          character: 'utf-8',
-        })
-  }
-  */
 };
