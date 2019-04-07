4067186f77ae17ea6c95ff0c565049204be8b139
Ingo Dahn <ingodahn@gmail.com>
2019-04-07 10:18:40 +0200

    Added SageCell Macros

--- assessment/libs/sagecell.php
+++ assessment/libs/sagecell.php
@@ -1,3 +1,31 @@
 <?php
-//does nothing
+//SageCell extension
+
+global $allowedmacros;
+array_push($allowedmacros,"sagecell", "sagecellTransfer");
+
+function sagecell($script) {
+	return "<pre class='converttosagecell'>".$script."</pre>";
+}
+
+function sagecellTransfer($script,$q,$label="Transfer",$part=-1) {
+if ($part < 0) {
+	$ref=$q-1;
+} else {
+	$ref=$q*1000+$part;
+}
+$fns="<script>function getSageOutput_$ref (el) {
+    var iframe=$(el).closest('.sagetransfer').find('.sagecellframe')[0];
+    var output=iframe.contentWindow.document.getElementsByClassName('sagecell_sessionOutput');
+    if (output.length) {
+	$('input[id=\'tc$ref\'],input[id=\'qn$ref\'').val(output[0].textContent);
+    } else {
+      alert('Please evaluate cell first');
+    }
+  }
+</script>";
+$btn="<input type='button' value='$label' onClick='getSageOutput_$ref(this)'>";
+
+return "<div class='sagetransfer'>".sagecell($script).$fns.$btn."</div>";
+}
 ?>
