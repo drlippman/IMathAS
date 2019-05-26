c05b7ca7c98052dad93cf4ad34baf794fcda231a
Ingo Dahn <ingodahn@gmail.com>
2019-05-26 04:53:40 +0200

    Merge with Davids repo. Sagecell Essay transfer not working yet

--- assessment/libs/sagecell.php
+++ assessment/libs/sagecell.php
@@ -2,10 +2,15 @@
 //SageCell extension
 
 global $allowedmacros;
-array_push($allowedmacros,"sagecell", "sagecellTransfer");
+array_push($allowedmacros,"sagecell", "sagecellEssay", "sagecellTransfer", "sagecellEssayTransfer");
 
 function sagecell($script) {
 	return "<pre class='converttosagecell'>".$script."</pre>";
+}
+
+function sagecellEssay($script,$essayPart=-1) {
+  $ansPart = $essayPart < 0 ? '$answerbox' : '$answerbox['.$essayPart.']';
+	return '<div class="converttosagecell">'."$ansPart".'<pre class="hidden">'.$script.'</pre></div>';
 }
 
 function sagecellTransfer($script,$q,$label="Transfer",$part=-1) {
@@ -27,5 +32,22 @@ $fns="<script>function getSageOutput_$ref (el) {
 $btn="<input type='button' value='$label' onClick='getSageOutput_$ref(this)'>";
 
 return "<div class='sagetransfer'>".sagecell($script).$fns.$btn."</div>";
+}
+
+function sagecellEssayTransfer($script,$q,$essayPart,$part,$label="Transfer") {
+  $ref=$q*1000+$part;
+  $fns="<script>function getSageOutput_$ref (el) {
+    var iframe=$(el).closest('.sagetransfer').find('.sagecellframe')[0];
+    var output=iframe.contentWindow.document.getElementsByClassName('sagecell_sessionOutput');
+    if (output.length) {
+	$('input[id=\'tc$ref\'],input[id=\'qn$ref\'').val(output[0].textContent);
+    } else {
+      alert('Please evaluate cell first');
+    }
+  }
+</script>";
+$btn="<input type='button' value='$label' onClick='getSageOutput_$ref(this)'>";
+
+return "<div class='sagetransfer'>".sagecellEssay($script,$essayPart).$fns.$btn."</div>";
 }
 ?>
