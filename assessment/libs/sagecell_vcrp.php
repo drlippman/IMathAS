f96ed659b32b9c124cd92c04f1769c482f01b571
Ingo Dahn <ingodahn@gmail.com>
2019-06-29 11:45:26 +0200

    Version 3.0 of sagecell_vcrp. Major restructuring, not backwards compatible

--- assessment/libs/sagecell_vcrp.php
+++ assessment/libs/sagecell_vcrp.php
@@ -2,161 +2,59 @@
 //SageCell extension
 
 global $allowedmacros;
-array_push($allowedmacros,"sagecell", "sagecellEssay", "sagecellTransfer", "sagecellTransferMulti" ,"sagecellEssayTransfer","sagecellEssayTransferMulti" );
+array_push($allowedmacros,"sagecell", "sagecellButton" );
 
-function sagecell($script) {
-	return "<pre class='converttosagecell'>".$script."</pre>";
-}
-
-function sagecellEssay($script,$essayPart=-1) {
-  $ansPart = $essayPart < 0 ? '[AB]' : '[AB'.$essayPart.']';
-	return '<div class="converttosagecell">'."$ansPart".'<pre class="hidden">'.$script.'</pre></div>';
-}
-
-function sagecellTransfer($script,$q,$label="Transfer",$part=-1,$format='any') {
-if ($part < 0) {
-	$ref=$q-1;
+function sagecell($script="",$q=0,$essayPart="none",$id="sagecell") {
+$cid='sagecell_vcrp_'.$id;
+$fns="<script>function getSageOutput_$id (part=-1,format='any') {
+    var ref=$q-1;
+    if (part >= 0)
+      ref=$q*1000+part;
+    var iframe=$(\"#\"+'$cid').find('.sagecellframe')[0];
+    var output=iframe.contentWindow.document.getElementsByClassName('sagecell_sessionOutput');
+    if (output.length) {
+      var sageOutput=output[0].textContent;
+      if (format == 'list') {
+        var start=sageOutput.indexOf('[');
+        var end=sageOutput.lastIndexOf(']');
+        if (start > -1 && end > -1)
+          sageOutput=sageOutput.substring(start+1,end);
+      }
+      if (format == 'matrix') {
+        sageOutput=sageOutput.replace(/ /g,',');
+        sageOutput=sageOutput.replace(/\[/g,'(');
+        sageOutput=sageOutput.replace(/\]/g,')');
+        sageOutput=sageOutput.replace(/\)\s*\(/g,'),(');
+        sageOutput='['+sageOutput+']';
+      }
+	    $('input[id=\'tc'+ref+'\'],input[id=\'qn'+ref+'\']').val(sageOutput);
+    } else {
+      alert('Please evaluate cell');
+    }
+  }
+</script>";
+if ($essayPart === 'none') {
+  return "<div id='$cid' class='sagetransfer'><pre class='converttosagecell'>".$script."</pre>".$fns."</div>";
 } else {
-	$ref=$q*1000+$part;
+  $ansPart = ($essayPart === "essay" ? '[AB]' : '[AB'.strval($essayPart).']');
+
+	return "<div id='$cid' class='sagetransfer'><div class='converttosagecell'>$ansPart<pre class='hidden'>$script</pre></div>$fns</div>";
 }
-$fns="<script>function getSageOutput_$ref (el) {
-    var iframe=$(el).closest('.sagetransfer').find('.sagecellframe')[0];
-    var output=iframe.contentWindow.document.getElementsByClassName('sagecell_sessionOutput');
-    if (output.length) {
-      var sageOutput=output[0].textContent;
-      if ('$format' == 'list') {
-        var start=sageOutput.indexOf('[');
-        var end=sageOutput.lastIndexOf(']');
-        if (start > -1 && end > -1)
-          sageOutput=sageOutput.substring(start+1,end);
-      }
-      if ('$format' == 'matrix') {
-        sageOutput=sageOutput.replace(/ /g,',');
-        sageOutput=sageOutput.replace(/\[/g,'(');
-        sageOutput=sageOutput.replace(/\]/g,')');
-        sageOutput=sageOutput.replace(/\)\s*\(/g,'),(');
-        sageOutput='['+sageOutput+']';
-      }
-	    $('input[id=\'tc$ref\'],input[id=\'qn$ref\'').val(sageOutput);
-    } else {
-      alert('Please evaluate cell');
-    }
+
+}
+
+function sagecellButton($label="Transfer",$part="none",$format='any',$id="sagecell") {
+  switch (strval($part)) {
+    case "none":
+      $parts=-1;
+      break;
+    case "essay":
+      $parts=-2;
+      break;
+    default:
+      $parts=$part;
   }
-</script>";
-$btn="<input type='button' value='$label' onClick='getSageOutput_$ref(this)'>";
-
-return "<div class='sagetransfer'>".sagecell($script).$fns.$btn."</div>";
-}
-
-function sagecellTransferMulti($script,$q,$labels,$parts,$formats=[]) {
-$fns=[];
-$btns=[];
-$fl=count($formats);
-for ($i=0;$i<count($parts);$i++) {
-  $ref=$q*1000+$parts[$i];
-  $fns[]="function getSageOutput_$ref (el) {
-    var iframe=$(el).closest('.sagetransfer').find('.sagecellframe')[0];
-    var output=iframe.contentWindow.document.getElementsByClassName('sagecell_sessionOutput');
-    if (output.length) {
-      var sageOutput=output[0].textContent;
-      if ($i < $fl && '$formats[$i]' == 'list') {
-        var start=sageOutput.indexOf('[');
-        var end=sageOutput.lastIndexOf(']');
-        if (start > -1 && end > -1)
-          sageOutput=sageOutput.substring(start+1,end);
-      }
-      if ($i < $fl && '$formats[$i]' == 'matrix') {
-        sageOutput=sageOutput.replace(/ /g,',');
-        sageOutput=sageOutput.replace(/\[/g,'(');
-        sageOutput=sageOutput.replace(/\]/g,')');
-        sageOutput=sageOutput.replace(/\)\s*\(/g,'),(');
-        sageOutput='['+sageOutput+']';
-      }
-	    $('input[id=\'tc$ref\'],input[id=\'qn$ref\'').val(sageOutput);
-    } else {
-      alert('Please evaluate cell');
-    }
-  }";
-  $label="Use Output as Answer to Part ".$parts[$i];
-  if ($labels[$i]) {
-    $label=$labels[$i];
-  }
-  $btns[]="<input type='button' value='$label' onClick='getSageOutput_$ref(this)'>";
-}
-$f=implode("\n",$fns);
-$b=implode("\n",$btns);
-return "<div class='sagetransfer'>".sagecell($script)."<script>".$f."</script>\n".$b."</div>";
-}
-
-function sagecellEssayTransfer($script,$q,$essayPart,$part,$label="Transfer",$format='any') {
-  $ref=$q*1000+$part;
-  $fns="<script>function getSageOutput_$ref (el) {
-    var iframe=$(el).closest('.sagetransfer').find('.sagecellframe')[0];
-    var output=iframe.contentWindow.document.getElementsByClassName('sagecell_sessionOutput');
-    if (output.length) {
-      var sageOutput=output[0].textContent;
-      if ('$format' == 'list') {
-        var start=sageOutput.indexOf('[');
-        var end=sageOutput.lastIndexOf(']');
-        if (start > -1 && end > -1)
-          sageOutput=sageOutput.substring(start+1,end);
-      }
-      if ('$format' == 'matrix') {
-        sageOutput=sageOutput.replace(/ /g,',');
-        sageOutput=sageOutput.replace(/\[/g,'(');
-        sageOutput=sageOutput.replace(/\]/g,')');
-        sageOutput=sageOutput.replace(/\)\s*\(/g,'),(');
-        sageOutput='['+sageOutput+']';
-      }
-	    $('input[id=\'tc$ref\'],input[id=\'qn$ref\'').val(sageOutput);
-    } else {
-      alert('Please evaluate cell first');
-    }
-  }
-</script>";
-$btn="<input type='button' value='$label' onClick='getSageOutput_$ref(this)'>";
-
-return "<div class='sagetransfer'>".sagecellEssay($script,$essayPart).$fns.$btn."</div>";
-}
-
-function sagecellEssayTransferMulti($script,$q,$essayPart, $parts,$labels,$formats=[]) {
-$fns=[];
-$btns=[];
-$fl=count($formats);
-for ($i=0;$i<count($parts);$i++) {
-  $ref=$q*1000+$parts[$i];
-  $fns[]="function getSageOutput_$ref (el) {
-    var iframe=$(el).closest('.sagetransfer').find('.sagecellframe')[0];
-    var output=iframe.contentWindow.document.getElementsByClassName('sagecell_sessionOutput');
-    if (output.length) {
-      var sageOutput=output[0].textContent;
-      if ($i < $fl && '$formats[$i]' == 'list') {
-        var start=sageOutput.indexOf('[');
-        var end=sageOutput.lastIndexOf(']');
-        if (start > -1 && end > -1)
-          sageOutput=sageOutput.substring(start+1,end);
-      }
-      if ($i < $fl && '$formats[$i]' == 'matrix') {
-        sageOutput=sageOutput.replace(/ /g,',');
-        sageOutput=sageOutput.replace(/\[/g,'(');
-        sageOutput=sageOutput.replace(/\]/g,')');
-        sageOutput=sageOutput.replace(/\)\s*\(/g,'),(');
-        sageOutput='['+sageOutput+']';
-      }
-	    $('input[id=\'tc$ref\'],input[id=\'qn$ref\'').val(sageOutput);
-    } else {
-      alert('Please evaluate cell');
-    }
-  }";
-  $label="Use Output as Answer to Part ".$parts[$i];
-  if ($labels[$i]) {
-    $label=$labels[$i];
-  }
-  $btns[]="<input type='button' value='$label' onClick='getSageOutput_$ref(this)'>";
-}
-$f=implode("\n",$fns);
-$b=implode("\n",$btns);
-return "<div class='sagetransfer'>".sagecellEssay($script,$essayPart)."<script>".$f."</script>\n".$b."</div>";
+  return "<input type='button' value='$label' onClick='getSageOutput_$id($parts,\"$format\")'>";
 }
 
 ?>
