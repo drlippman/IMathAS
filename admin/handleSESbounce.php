39b6168ec4766c6b5f51ebcb92f4d97fcda8829c
David Lippman <drlippman@yahoo.com>
2019-11-26 18:43:51 -0800

    Add email bounce handler for SES
    Squashed commit of the following:
    
    commit 6b2515aa4bf86cc57a1c665f6c8d2c7fc573ba7a
    Author: David Lippman <drlippman@yahoo.com>
    Date:   Tue Nov 26 18:41:27 2019 -0800
    
        Adjust display of bounced in email addresses
    
    commit d5ee919128a2bb95ed160aab7dafae937a5e5c3b
    Author: David Lippman <drlippman@gmail.com>
    Date:   Tue Nov 26 13:40:38 2019 -0800
    
        Start work on SES bounce handler

---
+++ admin/handleSESbounce.php
@@ -0,0 +1,61 @@
+<?php
+//IMathAS: handle email bounces from SES
+//(c) 2019 David Lippman
+
+/*
+  Note:  Per comment at https://stackoverflow.com/questions/15368095/php-json-decode-amazon-sns,
+  in SNS leave "Include Original Headers" disabled
+
+	Need to set $CFG['email']['authcode'] in config
+*/
+
+require("../init_without_validate.php");
+require("../includes/AWSSNSutil.php");
+
+if (php_sapi_name() == "cli") {
+	//running command line - no need for auth code
+} else if (!isset($CFG['email']['authcode'])) {
+	echo 'You need to set $CFG[\'email\'][\'authcode\'] in config.php';
+	exit;
+} else if (!isset($_GET['authcode']) || $CFG['email']['authcode']!=$_GET['authcode']) {
+	echo 'No authcode or invalid authcode provided';
+	exit;
+}
+
+if (isset($_SERVER['HTTP_X_AMZ_SNS_MESSAGE_TYPE'])) {
+	respondOK(); //send 200 response now
+}
+
+function disableEmail($email) {
+	global $DBH;
+	$stm = $DBH->prepare("UPDATE imas_users SET email=CONCAT('BOUNCED', email) WHERE email=?");
+	$stm->execute($email);
+}
+
+
+// Fetch the raw POST body containing the message
+$postBody = file_get_contents('php://input');
+
+// JSON decode the body to an array of message data
+$message = json_decode($postBody, true);
+$cnt = 0;
+if ($message) {
+	if ($message['notificationType'] == 'Bounce' &&
+		$message['bounce']['bounceType'] == 'Permanent'
+	) {
+		foreach ($message['bounce']['bouncedRecipients'] as $bouncers) {
+			if ($bouncers['action'] == 'failed') {
+				disableEmail($bouncers['emailAddress']);
+				$cnt++;
+			}
+		}
+	}
+	if ($message['notificationType'] == 'Complaint') {
+		foreach ($message['complaint']['complainedRecipients'] as $bouncers) {
+			disableEmail($bouncers['emailAddress']);
+			$cnt++;
+		}
+	}
+}
+
+echo "Done; $cnt updated";
