927ab22b0f25e3bf94b43d48a41169c161558c3b
Ingo Dahn <ingodahn@gmail.com>
2020-02-19 19:44:26 +0100

    styling timerbox, adding info/studentTOS

---
+++ info/studentTOS.php
@@ -0,0 +1,7 @@
+<?php
+echo '<h2>Nutzungsbedingungen und Datenschutzerklärung</h2>
+<ul>
+<li><a href="https://netmath.vcrp.de/downloads/Systeme/NutzungsbedingungenIMathAS.html" target="_blank">Nutzungsbedingungen</a></li>
+<li><a href="https://netmath.vcrp.de/downloads/Systeme/privacyIMathAS.html" target="_blank">Datenschutzerklärung</a></li>
+</uk>';
+?>
