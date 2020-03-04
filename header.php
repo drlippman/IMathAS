c296bbe9e5dba359ee98b6d1576dbab30bfcc12c
Alena Holligan <alena@lumenlearning.com>
2020-03-04 10:45:32 -0800

    ohm-232 and ohm-236 update imath no hooks needed

--- header.php
+++ header.php
@@ -258,7 +258,7 @@ function getactivetab() {
 	}
 	return $a;
 }
-if (isset($cid) && !isset($flexwidth) && !isset($hideAllHeaderNav)) {
+if (isset($cid) && !isset($flexwidth) && !isset($hideAllHeaderNav) && !isset($nocoursenav)) {
 	echo '<div id="navlistcont" role="navigation" aria-label="'._('Course Navigation').'">';
 	echo '<ul id="navlist">';
 	$a = array('course'=>'', 'msg'=>'', 'forum'=>'', 'cal'=>'', 'gb'=>'', 'roster'=>'');
