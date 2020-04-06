b5dd9549fa306c095d8f96873374a9a882dab551
drlippman <drlippman@yahoo.com>
2020-04-06 12:51:40 -0700

    Force reload

--- assess2/gbviewassess.php
+++ assess2/gbviewassess.php
@@ -2,7 +2,7 @@
 // IMathAS: Assess2 gradebook details page
 // (c) 2019 David Lippman
 
-$lastupdate = '20200404';
+$lastupdate = '20200406';
 
 require('../init.php');
 if (empty($_GET['cid']) || empty($_GET['aid'])) {
