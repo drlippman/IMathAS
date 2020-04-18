cbb9033ba0587040d8c4fd47ff4eef6e8e7fc13a
drlippman <drlippman@yahoo.com>
2020-04-17 18:35:08 -0700

    Rebuild Vue

--- assess2/index.php
+++ assess2/index.php
@@ -2,7 +2,7 @@
 // IMathAS: Main launch page for assess2 assessment player
 // (c) 2019 David Lippman
 
-$lastupdate = '20200415';
+$lastupdate = '20200417';
 
 require('../init.php');
 if (empty($_GET['cid']) || empty($_GET['aid'])) {
