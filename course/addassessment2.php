03711a126a616cf92f09c92b2ad403294e808ef5
drlippman <drlippman@yahoo.com>
2020-04-19 18:43:04 -0700

    Add copy summary/intro/dates options on copy options from in addassess2 IM-742

--- course/addassessment2.php
+++ course/addassessment2.php
@@ -169,6 +169,31 @@ if (!(isset($teacherid))) { // loaded by a NON-teacher
 					$toset[$k] = $v;
 				}
 			}
+			if (isset($_POST['copysummary']) || isset($_POST['copyinstr']) ||
+				isset($_POST['copydates']) || isset($_POST['copyendmsg'])
+			) {
+				$stm = $DBH->prepare("SELECT summary,intro,startdate,enddate,reviewdate,avail,endmsg FROM imas_assessments WHERE id=:id AND courseid=:cid");
+				$stm->execute(array(':id'=>intval($_POST['copyfrom']), ':cid'=>$cid));
+				$row = $stm->fetch(PDO::FETCH_ASSOC);
+				if (isset($_POST['copysummary'])) {
+					$toset['summary'] = $row['summary'];
+				}
+				if (isset($_POST['copyinstr'])) {
+					if (($introjson=json_decode($row['intro']))!==null) { //is json intro
+							$toset['intro'] = $introjson[0];
+					} else {
+							$toset['intro'] = $row['intro'];
+					}
+				}
+				if (isset($_POST['copydates'])) {
+					$toset['startdate'] = $row['startdate'];
+					$toset['enddate'] = $row['enddate'];
+					$toset['reviewdate'] = $row['reviewdate'];
+				}
+				if (isset($_POST['copyendmsg'])) {
+					$toset['endmsg'] = $row['endmsg'];
+				}
+			}
 		} else { // set using values selected
 			$toset['displaymethod'] = Sanitize::stripHtmlTags($_POST['displaymethod']);
 
