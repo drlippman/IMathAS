6c013b5a6c1f5aa4ca999d91ff74ea60f00ca2fd
drlippman <drlippman@yahoo.com>
2020-05-25 17:48:02 -0700

    Replace normalcdf with faster one

--- assessment/libs/stats.php
+++ assessment/libs/stats.php
@@ -973,7 +973,7 @@ function boxplot($arr,$label="",$options = array()) {
 //calculates the area under the standard normal distribution to the left of the
 //z-value z, to dec decimals (defaults to 4, max of 10)
 //based on someone else's code - can't remember whose!
-function normalcdf($ztest,$dec=4) {
+/*function normalcdf($ztest,$dec=4) {
 	if (!is_finite($ztest)) {
 		echo 'invalid value for z';
 		return 0;
@@ -1031,6 +1031,47 @@ function normalcdf($ztest,$dec=4) {
 		$pval = round(1-$eps,$dec);
 	}
 	return $pval;
+}
+*/
+// port from jStat, MIT License
+function erf($x) {
+  $cof = [-1.3026537197817094, 6.4196979235649026e-1, 1.9476473204185836e-2,
+             -9.561514786808631e-3, -9.46595344482036e-4, 3.66839497852761e-4,
+             4.2523324806907e-5, -2.0278578112534e-5, -1.624290004647e-6,
+             1.303655835580e-6, 1.5626441722e-8, -8.5238095915e-8,
+             6.529054439e-9, 5.059343495e-9, -9.91364156e-10,
+             -2.27365122e-10, 9.6467911e-11, 2.394038e-12,
+             -6.886027e-12, 8.94487e-13, 3.13092e-13,
+             -1.12708e-13, 3.81e-16, 7.106e-15,
+             -1.523e-15, -9.4e-17, 1.21e-16,
+             -2.8e-17];
+  $isneg = false;
+  $d = 0;
+  $dd = 0;
+
+  if ($x < 0) {
+    $x = -$x;
+    $isneg = true;
+  }
+
+  $t = 2 / (2 + $x);
+  $ty = 4 * $t - 2;
+
+  for($j = count($cof) - 1; $j > 0; $j--) {
+    $tmp = $d;
+    $d = $ty * $d - $dd + $cof[$j];
+    $dd = $tmp;
+  }
+
+  $res = $t * exp(-$x * $x + 0.5 * ($cof[0] + $ty * $d) - $dd);
+  return $isneg ? $res - 1 : 1 - $res;
+}
+function normalcdf($z,$dec=4) {
+  if (!is_finite($ztest)) {
+		echo 'invalid value for z';
+		return 0;
+	}
+  return round(0.5 * (1 + erf(($z) / sqrt(2))), $dec);
 }
 
 //tcdf(t,df,[dec])
