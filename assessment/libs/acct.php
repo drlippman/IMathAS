96f5e1737810fd3097c2418c0b427f5edcb5634d
drlippman <drlippman@yahoo.com>
2020-06-13 19:04:31 -0700

    add underline opts for makeaccttable3

--- assessment/libs/acct.php
+++ assessment/libs/acct.php
@@ -817,6 +817,8 @@ function makeaccttable2($headers, $coltypes, $fixedrows, $cols, $sn, &$anstypes,
 //opts: optionsal array of options:
 //   $opts['totrow']: row to treat as totals row (decorates above and below with lines) - optional
 //   $opts['class']: class to use for table
+//   $opts['underline']: array of row=>underline style: 1 single, 2 double
+//      can also do row=>[style, column] or row=>[style, [column, column]]     
 //   $opts['questions'][n] = array of pull-down options for column n
 function makeaccttable3($headers, $coltypes, $fixedrows, $cols, $sn, &$anstypes, &$answer, &$showanswer, &$displayformat, &$questions, &$answerformat, &$answerboxsize, $opts=array()) {
 	if ($anstypes === null) { $anstypes = array();}
@@ -826,14 +828,18 @@ function makeaccttable3($headers, $coltypes, $fixedrows, $cols, $sn, &$anstypes,
 	if (isset($opts['totrow'])) { $totrow = $opts['totrow'];} else {$totrow = -1;}
 	if (isset($opts['class'])) { $tblclass = $opts['class'];} else {$tblclass = 'gridded';}
 
-	$maxsize = array();  $hasdecimals = false;
+	$maxsize = array();  $hasdecimals = false;  $rowcnt = 0;
+	// handle sparse arrays
+	for ($i=0;$i<count($cols);$i++) {
+		$rowcnt = max($rowcnt, max(array_keys($cols[$i]))+1);
+	}
 	for ($j=0;$j<count($coltypes);$j++) {
 		if ($coltypes[$j]==false || $coltypes[$j]==-1) {continue;} //fixed column
 		$maxsize[$j] = 0;
-		for ($i=0;$i<count($cols[$j]);$i++) {
-			$sl = strlen($cols[$j][$i]);
+		foreach ($cols[$j] as $v) {
+			$sl = strlen($v);
 			if ($sl>$maxsize[$j]) { $maxsize[$j] = $sl;}
-			if (!$hasdecimals && strpos($cols[$j][$i],'.')!==false) { $hasdecimals = true;}
+			if (!$hasdecimals && strpos($v, '.')!==false) { $hasdecimals = true;}
 		}
 		$maxsize[$j] += floor(($maxsize[$j]-0.5)/3);  //add size to account for commas
 	}
@@ -872,13 +878,24 @@ function makeaccttable3($headers, $coltypes, $fixedrows, $cols, $sn, &$anstypes,
 		}
 		$out .= '</tr>';  $sa .= '</tr>';
 	}
-	for ($i=0;$i<count($cols[0]);$i++) {
+	for ($i=0;$i<$rowcnt;$i++) {
 		$out .= '<tr>';  $sa .= '<tr>';
 		for ($j=0;$j<count($coltypes);$j++) {
 			if ($i+1==$totrow) {
 				$dec = ' style="border-bottom: 3px double #000;"';
 			} else if ($i==$totrow) {
 				$dec = ' style="border-bottom: 3px double #000;"';
+			} else if (!empty($opts['underline'][$i]) &&
+				(!is_array($opts['underline'][$i]) ||
+				(is_array($opts['underline'][$i][1]) && in_array($j, $opts['underline'][$i][1])) ||
+				(!is_array($opts['underline'][$i][1]) && $j == $opts['underline'][$i][1]))
+			) {
+				$ustyle = is_array($opts['underline'][$i]) ? $opts['underline'][$i][0] : $opts['underline'][$i];
+				if ($ustyle == 2) {
+					$dec = ' style="border-bottom: 3px double #000;"';
+				} else {
+					$dec = ' style="border-bottom: 1px solid #000;"';
+				}
 			} else {
 				$dec = '';
 			}
@@ -888,12 +905,16 @@ function makeaccttable3($headers, $coltypes, $fixedrows, $cols, $sn, &$anstypes,
 				$sa .= "<td$dec>".$cols[$j][$i].'</td>';
 
 			} else {
-				if ($i==$totrow && !isset($cols[$j][$i])) {
-					$thistot = 0;
-					for ($k=0;$k<$totrow;$k++) {
-						$thistot += $cols[$j][$k];
+				if (!isset($cols[$j][$i])) {
+					if ($i==$totrow) {
+						$thistot = 0;
+						for ($k=0;$k<$totrow;$k++) {
+							$thistot += $cols[$j][$k];
+						}
+						$cols[$j][$i] = $thistot;
+					} else {
+						$cols[$j][$i] = '';
 					}
-					$cols[$j][$i] = $thistot;
 				}
 
 				if ($coltypes[$j]<0) {
