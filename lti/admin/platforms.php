8f726761bbb3c60d2f7dae0705eb1babc7d32b82
drlippman <drlippman@yahoo.com>
2020-07-03 15:37:57 -0700

    Tweaks to platforms page

--- lti/admin/platforms.php
+++ lti/admin/platforms.php
@@ -59,20 +59,39 @@ while ($row = $stm->fetch(PDO::FETCH_ASSOC)) {
 echo '</ul>';
 
 echo '<h2>'._('New Platform').'</h2>';
-echo '<p>'._('Info to put in the LSM').'</p>';
+echo '<p>'._('Info to put in the LMS').'</p>';
 echo '<ul class=nomark>';
-echo '<li>'._('Target Link URI/Tool URL/Redirect URI:').' '.$basesiteurl.'/lti/launch.php</li>';
-echo '<li>'._('OpenID Connect / Initiate Login URL:').' '.$basesiteurl.'/lti/login.php</li>';
-echo '<li>'._('Keyset URL:').' '.$basesiteurl.'/lti/jwks.php</li>';
+echo '<li>'._('Target Link URI/Tool URL/Redirect URI:').' <span class=tocopy>'.$basesiteurl.'/lti/launch.php</span></li>';
+echo '<li>'._('OpenID Connect / Initiate Login URL:').' <span class=tocopy>'.$basesiteurl.'/lti/login.php</span></li>';
+echo '<li>'._('Keyset URL:').' <span class=tocopy>'.$basesiteurl.'/lti/jwks.php</span></li>';
 echo '</ul>';
-echo '<p>'._('Info from LSM').'</p>';
+echo '<p>'._('Info from LMS').'</p>';
 echo '<ul class=nomark>';
-echo '<li>'._('Issuer/Platform ID:').' <input name=issuer size=50/></li>';
-echo '<li>'._('Client ID:').' <input name=clientid size=50/></li>';
-echo '<li>'._('Keyset URL:').' <input name=keyseturl size=50/></li>';
-echo '<li>'._('Token URL:').' <input name=tokenurl size=50/></li>';
-echo '<li>'._('Authentication URL:').' <input name=authurl size=50/></li>';
+echo '<li><label>'._('Issuer/Platform ID:').' <input name=issuer size=50/></label></li>';
+echo '<li><label>'._('Client ID:').' <input name=clientid size=50/></label></li>';
+echo '<li><label>'._('Keyset URL:').' <input name=keyseturl size=50/></label></li>';
+echo '<li><label>'._('Token URL:').' <input name=tokenurl size=50/></label></li>';
+echo '<li><label>'._('Authentication URL:').' <input name=authurl size=50/></label></li>';
 echo '</ul>';
 echo '<button type=submit>'._('Add Platform').'</button></p>';
 echo '</form>';
+?>
+<script type="text/javascript">
+$(function() {
+  $('.tocopy').each(function(i,eltocopy) {
+    $(eltocopy).after($('<button>', {type: 'button', text: '<?php echo _('Copy');?>'})
+      .on('click', function() {
+        var el = document.createElement('textarea');
+        el.value = $(this).prev().text();
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+      }));
+  });
+});
+</script>
+<?php
 require('../../footer.php');
