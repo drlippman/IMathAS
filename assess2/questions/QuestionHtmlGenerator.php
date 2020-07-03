539f3ef70ff190e8fe56b6edb989c1e4ed2268ee
drlippman <drlippman@yahoo.com>
2020-07-02 18:38:52 -0700

    Bug fix: prevent halting error on missing $anstypes

--- assess2/questions/QuestionHtmlGenerator.php
+++ assess2/questions/QuestionHtmlGenerator.php
@@ -262,9 +262,11 @@ class QuestionHtmlGenerator
 
         if ($quesData['qtype'] == "multipart" || $quesData['qtype'] == 'conditional') {
             // $anstypes is question writer defined.
-            if (!isset($anstypes) && $GLOBALS['myrights'] > 10) {
+            if (!isset($anstypes)) {
+              if ($GLOBALS['myrights'] > 10) {
                 $this->addError('Error in question: missing $anstypes for multipart or conditional question');
-                $anstypes = array("number");
+              }
+              $anstypes = array("number");
             }
 
             // Calculate answer weights.
