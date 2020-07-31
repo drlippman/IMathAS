bf2de37961ab00487a52ac22136abb41fd26a0c7
drlippman <drlippman@yahoo.com>
2020-07-31 14:32:51 -0700

    Bug fix migration

--- migrations/20200530_add_ipeds.php
+++ migrations/20200530_add_ipeds.php
@@ -6,18 +6,18 @@ $DBH->beginTransaction();
 $query = 'CREATE TABLE `imas_ipeds` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `type` char(1) NOT NULL,
-  `id` VARCHAR(32) NOT NULL,
+  `ipedsid` VARCHAR(32) NOT NULL,
   `school` varchar(255) NOT NULL DEFAULT "",
   `agency` varchar(255) NOT NULL DEFAULT "",
   `country` char(2) NOT NULL DEFAULT "US",
   `state` char(2) NOT NULL DEFAULT "",
   `zip` int(5),
-  INDEX (`type`,`id`),
-  FULLTEXT `school` (`school`)
-  FULLTEXT `agency` (`agency`)
+  INDEX (`type`,`ipedsid`),
+  FULLTEXT `school` (`school`),
+  FULLTEXT `agency` (`agency`),
   INDEX `zip` (`zip`),
   INDEX `loc` (`country`,`state`)
-) ENGINE=InnoDB;';
+) CHARACTER SET UTF8 COLLATE utf8_general_ci ENGINE=InnoDB;';
 $res = $DBH->query($query);
 if ($res===false) {
     echo "<p>Query failed: ($query) : ".$DBH->errorInfo()."</p>";
@@ -31,7 +31,7 @@ $query = 'CREATE TABLE `imas_ipeds_group` (
   `groupid` INT(10) NOT NULL,
   PRIMARY KEY `link` (`type`,`ipedsid`,`groupid`),
   INDEX `groupid` (`groupid`)
-) ENGINE=InnoDB;';
+) CHARACTER SET UTF8 COLLATE utf8_general_ci ENGINE=InnoDB;';
 $res = $DBH->query($query);
 if ($res===false) {
     echo "<p>Query failed: ($query) : ".$DBH->errorInfo()."</p>";
