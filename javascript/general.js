2fca8ec5c79cc4e7896709a9421b602b4dd97838
drlippman <drlippman@yahoo.com>
2020-08-17 12:17:11 -0700

    Allow style in iframe in tinymce

--- javascript/general.js
+++ javascript/general.js
@@ -484,7 +484,7 @@ function initeditor(edmode,edids,css,inline,setupfunction){
 		menubar: false,//"edit insert format table tools ",
 		toolbar1: "myEdit myInsert styleselect | bold italic underline subscript superscript | forecolor backcolor | snippet code | saveclose",
 		toolbar2: " alignleft aligncenter alignright | bullist numlist outdent indent  | attach link unlink image | table | asciimath asciimathcharmap asciisvg",
-		extended_valid_elements : 'iframe[src|width|height|name|align|allowfullscreen|frameborder],param[name|value],@[sscr]',
+		extended_valid_elements : 'iframe[src|width|height|name|align|allowfullscreen|frameborder|style],param[name|value],@[sscr]',
 		content_css : imasroot+(cssmode==1?'/assessment/mathtest.css,':'/imascore.css,')+imasroot+'/themes/'+coursetheme,
 		AScgiloc : imasroot+'/filter/graph/svgimg.php',
 		convert_urls: false,
