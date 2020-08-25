5e077811a778d8d9d9082db2e994527de760348b
drlippman <drlippman@yahoo.com>
2020-08-25 13:13:58 -0700

    Bug fix: tiny dialog pos still incorrect in Bb

--- header.php
+++ header.php
@@ -180,7 +180,7 @@ if (isset($_SESSION['graphdisp']) && $_SESSION['graphdisp']==1) {
 
 
 if (isset($useeditor) && $_SESSION['useed']==1) {
-    echo '<script type="text/javascript" src="'.$imasroot.'/tinymce4/tinymce_bundled.min.js?v=071320"></script>';
+    echo '<script type="text/javascript" src="'.$imasroot.'/tinymce4/tinymce_bundled.min.js?v=082520"></script>';
     //echo '<script type="text/javascript" src="'.$imasroot.'/tinymce4/tinymce.js?v=051919"></script>';
 
 	echo "\n";
