a67ad2b458331730d5a2de0abdc010f8d7e22763
drlippman <drlippman@yahoo.com>
2020-09-09 19:39:01 -0700

    Update javascript version markers

--- header.php
+++ header.php
@@ -66,7 +66,7 @@ div.breadcrumb { display:none;}
 <script type="text/javascript">
 var imasroot = '<?php echo $imasroot; ?>'; var cid = <?php echo (isset($cid) && is_numeric($cid))?$cid:0; ?>;
 </script>
-<script type="text/javascript" src="<?php echo $imasroot;?>/javascript/general.js?v=082920b"></script>
+<script type="text/javascript" src="<?php echo $imasroot;?>/javascript/general.js?v=090920"></script>
 <?php
 //$_SESSION['mathdisp'] = 3;
 //
