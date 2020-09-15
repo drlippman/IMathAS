9556689fd0601f7d75cf9ee33355ec50b0b3f9ca
drlippman <drlippman@yahoo.com>
2020-09-14 23:03:58 -0700

    Bug fix: Written Example preventing question helps from displaying in preview

--- javascript/assess2supp.js
+++ javascript/assess2supp.js
@@ -191,20 +191,23 @@ function disableInputs(qn, disabled) {
      out += '<li>'+_('Question Help')+':</li>';
      for (let help of helps) {
        let title = help.label;
+       let icon = '<svg viewBox="0 0 24 24" width="16" height="16" stroke="black" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>';
        if (help.label == 'video') {
          title = _('Video');
+         icon = '<svg viewBox="0 0 24 24" width="16" height="16" stroke="black" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"></path><polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"></polygon></svg>';
        } else if (help.label == 'read') {
          title = _('Read');
        } else if (help.label == 'ex') {
          title = _('Written Example');
        }
-       out += '<li><a target="qhelp" href="'+ help['url'] +'"';
-       if (help['descr'] != '') {
-        help['descr'] = help['descr'].replace(/"/g,'&quot;').replace(/'/g,'&#039;');
-        out += ' title="'+help['descr']+'"';
-        out += ' aria-label="'+title+' '+help['descr']+'"';
-     }
-       out += '>' + title + '</a></li>';
+
+       out += '<li><a target="qhelp" href="'+ help.url +'"';
+       if (help.descr && help.descr != '') {
+        help.descr = help.descr.replace(/"/g,'&quot;').replace(/'/g,'&#039;');
+        out += ' title="'+help.descr+'"';
+        out += ' aria-label="'+title+' '+help.descr+'"';
+       }
+       out += '>' + icon + ' ' + title + '</a></li>';
      }
      out += '</ul>';
      $(qwrap).append($("<div>", {class: "qhelps"}).html(out));
