f33856fe34dd3c0f385ebceafa6ddc83773f87f9
drlippman <drlippman@yahoo.com>
2020-09-24 12:55:10 -0700

    Rebuild Vue and JS

--- course/testquestion2.php
+++ course/testquestion2.php
@@ -182,7 +182,7 @@ if (!empty($CFG['assess2-use-vue-dev'])) {
   $placeinhead .= '<script src="'.$staticroot.'/mathquill/mqedlayout.js?v=041920" type="text/javascript"></script>';
 } else {
   $placeinhead .= '<script src="'.$staticroot.'/mathquill/mathquill.min.js?v=022720" type="text/javascript"></script>';
-  $placeinhead .= '<script src="'.$staticroot.'/javascript/assess2_min.js?v=072520" type="text/javascript"></script>';
+  $placeinhead .= '<script src="'.$staticroot.'/javascript/assess2_min.js?v=092420" type="text/javascript"></script>';
 }
 
 $placeinhead .= '<script src="'.$staticroot.'/javascript/assess2supp.js?v=091420" type="text/javascript"></script>';
