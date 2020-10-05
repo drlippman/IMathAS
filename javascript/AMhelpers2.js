bde9885ab8f102c8ab5af9223a1b2f07593efa52
drlippman <drlippman@yahoo.com>
2020-10-05 10:22:59 -0700

    Bug fix negative numbers giving warning in "number" type

--- javascript/AMhelpers2.js
+++ javascript/AMhelpers2.js
@@ -1176,7 +1176,7 @@ function processNumber(origstr, format) {
                 err += _('This is not an integer.');
             }
         } else {
-            if (!str.match(/^\s*(\d+\.?\d*|\.\d+|\d\.?\d*\s*E\s*[\-\+]?\d+)\s*$/)) {
+            if (!str.match(/^\s*\-?(\d+\.?\d*|\.\d+|\d\.?\d*\s*E\s*[\-\+]?\d+)\s*$/)) {
                 err += _('This is not a decimal or integer value.');
             }
         }
