cb3cf09eae724a86a7d4da8874229a9c2d9d91f7
drlippman <drlippman@yahoo.com>
2020-10-29 09:58:25 -0700

    Adjust isolategradeassess's IP markers to match main gradebooks, considering timelimit

--- course/isolateassessgrade.php
+++ course/isolateassessgrade.php
@@ -185,7 +185,7 @@
 	if ($aver>1) {
 		$query = "SELECT iu.LastName,iu.FirstName,istu.section,istu.code,istu.timelimitmult,";
 		$query .= "IF((iar.status&1)=1,iar.scoreddata,'') AS scoreddata,";
-		$query .= "istu.userid,iar.score,iar.starttime,iar.lastchange,iar.timeontask,iar.status,istu.locked FROM imas_users AS iu JOIN imas_students AS istu ON iu.id = istu.userid AND istu.courseid=:courseid ";
+		$query .= "istu.userid,iar.score,iar.starttime,iar.lastchange,iar.timeontask,iar.status,iar.timelimitexp,istu.locked FROM imas_users AS iu JOIN imas_students AS istu ON iu.id = istu.userid AND istu.courseid=:courseid ";
 		$query .= "LEFT JOIN imas_assessment_records AS iar ON iu.id=iar.userid AND iar.assessmentid=:assessmentid WHERE istu.courseid=:courseid2 ";
 	} else {
 		$query = "SELECT iu.LastName,iu.FirstName,istu.section,istu.code,istu.timelimitmult,";
@@ -304,9 +304,20 @@
 			$total = $line['score'];
 			$timeused = $line['lastchange'] - $line['starttime'];
 			$timeontask = round($line['timeontask']/60,1);
-			$isOvertime = ($line['status']&4) == 4;
-			$IP = ($line['status']&3)>0;
-			$UA = ($line['status']&1)>0;
+            $isOvertime = ($line['status']&4) == 4;
+            $IP = 0;
+            $UA = 0;
+            if (($line['status']&1)>0 && ($line['thisenddate']<$now ||  //unsubmitted by-assess, and due date passed
+                ($line['timelimitexp']>0 && $line['timelimitexp']<$now)) // or time limit expired on last att
+            ) {
+                $UA=1;
+            } else if (($line['status']&3)>0 && $line['thisenddate']>$now && // unsubmitted attempt any mode and before due date
+                ($line['timelimitexp']==0 || $line['timelimitexp']>$now) // and time limit not expired
+            ) {
+                $IP=1;
+            }
+			//$IP = ($line['status']&3)>0;
+			//$UA = ($line['status']&1)>0;
 		} else {
 			$total = 0;
 			$sp = explode(';',$line['bestscores']);
@@ -390,9 +401,9 @@
 			//if ($total<$minscore) {
 			if (($minscore<10000 && $total<$minscore) || ($minscore>10000 && $total<($minscore-10000)/100*$totalpossible)) {
 				echo "&nbsp;(NC)";
-			} else 	if ($IP==1 && $line['thisenddate'] > $now) {
+			} else 	if ($IP==1) {
 				echo "&nbsp;(IP)";
-			} else 	if ($UA==1 && $line['thisenddate'] < $now) {
+			} else 	if ($UA==1) {
 				echo "&nbsp;(UA)";
 			} else	if ($isOvertime) {
 				echo "&nbsp;(OT)";
