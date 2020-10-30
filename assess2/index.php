1b6950706b53ad2ca476eaa67799663c264c5bba
drlippman <drlippman@yahoo.com>
2020-10-30 15:29:14 -0700

    Bug fix choices with html not displaying right in livepoll results

--- assess2/index.php
+++ assess2/index.php
@@ -2,7 +2,7 @@
 // IMathAS: Main launch page for assess2 assessment player
 // (c) 2019 David Lippman
 
-$lastupdate = '20201002';
+$lastupdate = '20201030';
 
 require('../init.php');
 if (empty($_GET['cid']) || empty($_GET['aid'])) {
