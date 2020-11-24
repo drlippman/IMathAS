14731484858706750066820af25a642be023ed0f
David Lippman <drlippman@yahoo.com>
2020-11-24 13:32:57 -0800

    Bug fix: update assessmen_min script version tag in old assess header

--- assessment/header.php
+++ assessment/header.php
@@ -96,7 +96,7 @@ if (!empty($CFG['use_csrfp']) && class_exists('csrfProtector')) {
 	echo csrfProtector::output_header_code();
 }
 
-echo '<script src="' . $staticroot . '/javascript/assessment_min.js?v=092020" type="text/javascript"></script>';
+echo '<script src="' . $staticroot . '/javascript/assessment_min.js?v=112420" type="text/javascript"></script>';
 
 
 //assessment_min.js bundles: general.js, mathjs.js, AMhelpers.js, confirmsubmit.js, drawing.js, and eqntips.js
