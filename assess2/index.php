58066d4f2a5140165795831988036d40049d9c92
drlippman <drlippman@yahoo.com>
2020-12-11 10:40:29 -0800

    Bug fix: livepoll timer not working

--- assess2/index.php
+++ assess2/index.php
@@ -2,7 +2,7 @@
 // IMathAS: Main launch page for assess2 assessment player
 // (c) 2019 David Lippman
 
-$lastupdate = '20201204';
+$lastupdate = '20201211';
 
 require('../init.php');
 if (empty($_GET['cid']) || empty($_GET['aid'])) {
