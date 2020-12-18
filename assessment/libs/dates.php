d01ca19a6d482ec609e092c20483917d4ae37c27
sussexgooner <d.brown456@icloud.com>
2020-12-18 15:29:39 -0700

    Changed function names by replacing uppercase letters with lowercase. Added documentation regarding the behavior of dates_diffdays with partial days.
    dates_diffdays with dates with embedded times ignores the times. For example, the difference between today at 3pm and tomorrow at 3am is 1 day as is the difference between today at 3pm and tomorrow at 6pm.

--- assessment/libs/dates.php
+++ assessment/libs/dates.php
@@ -4,102 +4,102 @@
 //          University of Colorado Boulder
 
 global $allowedmacros;
-array_push($allowedmacros,"dates_addDays","dates_addWeeks","dates_addMonths","dates_addYears","dates_EOMonth",
-"dates_BOMonth","dates_diffDays","dates_randdate","dates_dateFormat");
+array_push($allowedmacros,"dates_adddays","dates_addweeks","dates_addmonths","dates_addyears","dates_eomonth",
+"dates_bomonth","dates_diffdays","dates_randdate","dates_dateformat");
 
-// function addDays
+// function adddays
 // adds a number of days to an existing date
 // uses $fmt as format of date inputs and outputs
-function dates_addDays($date, $days,$fmt = "F j, Y"){
+function dates_adddays($date, $days,$fmt = "F j, Y"){
 	$datetime = date_create_from_Format($fmt,$date);
 	if ($datetime==false) {
-		echo "date_addDays::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
+		echo "date_adddays::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
 		return false;
 	}
 	$datetime->modify('+' . $days .' days');
 	return $datetime->format($fmt);
 }
 
-// function addWeeks
+// function addweeks
 // adds a number of weeks to an existing date
 // uses $fmt as format of date inputs and outputs
-function dates_addWeeks($date, $weeks,$fmt = "F j, Y"){
+function dates_addweeks($date, $weeks,$fmt = "F j, Y"){
 	$datetime = date_create_from_Format($fmt,$date);
 	if ($datetime==false) {
-		echo "date_addWeeks::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
+		echo "date_addweeks::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
 		return false;
 	}
 	$datetime->modify('+' . $weeks .' weeks');
 	return $datetime->format($fmt);
 }
 
-// function addMonths
+// function addmonths
 // adds a number of months to an existing date
 // uses $fmt as format of date inputs and outputs
-function dates_addMonths($date,$months,$fmt= "F j, Y"){
+function dates_addmonths($date,$months,$fmt= "F j, Y"){
 	$datetime = date_create_from_Format($fmt,$date);
 	if ($datetime==false) {
-		echo "date_addMonths::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
+		echo "date_addmonths::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
 		return false;
 	}
 	$datetime->modify('+' . $months .' months');
 	return $datetime->format($fmt);
 }
 
-// function addYears
+// function addyears
 // adds a number of weeks to an existing date
 // uses $fmt as format of date inputs and outputs
-function dates_addYears($date, $years,$fmt = "F j, Y"){
+function dates_addyears($date, $years,$fmt = "F j, Y"){
 	$datetime = date_create_from_Format($fmt,$date);
 	if ($datetime==false) {
-		echo "date_addYears::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
+		echo "date_addyears::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
 		return false;
 	}
 	$datetime->modify('+' . $years .' Years');
 	return $datetime->format($fmt);
 }
 
-// function EOMonth
+// function eomonth
 // returns the last date of the month
 // from a given date. 
 // uses $fmt as format of date inputs and outputs
-function dates_EOMonth($date,$fmt="F j, Y") {
+function dates_eomonth($date,$fmt="F j, Y") {
 	$datetime = date_create_from_Format($fmt,$date);
 	if ($datetime==false) {
-		echo "date_EOMonth::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
+		echo "date_eomonth::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
 		return false;
 	}
 	$datetime->modify('last day of this month');
 	return $datetime->format($fmt);
 }
 
-// function BOMonth
+// function bomonth
 // returns the first date of the month
 // from a given date. 
 // uses $fmt as format of date inputs and outputs
-function dates_BOMonth($date,$fmt="F j, Y") {
+function dates_bomonth($date,$fmt="F j, Y") {
 	$datetime = date_create_from_Format($fmt,$date);
 	if ($datetime==false) {
-		echo "date_BOMonth::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
+		echo "date_bomonth::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
 		return false;
 	}
 	$datetime->modify('first day of this month');
 	return $datetime->format($fmt);
 }
 
-// function diffDays
+// function diffdays
 // returns the number of days between
 // date1 and date2. Positive if date2>date1
 // uses $fmt as format of date inputs and outputs
-function dates_diffDays($date1,$date2,$fmt="F j, Y") {
+function dates_diffdays($date1,$date2,$fmt="F j, Y") {
 	$dt1 = date_create_from_format($fmt,$date1);
 	if ($dt1==false) {
-		echo "date_diffDays::could not create date from string. The date is " . $date1 . " and the format is " . $fmt . ".";
+		echo "date_diffdays::could not create date from string. The date is " . $date1 . " and the format is " . $fmt . ".";
 		return false;
 	}
 	$dt2 = date_create_from_format($fmt,$date2);
 	if ($dt2==false) {
-		echo "date_diffDays::could not create date from string. The date is " . $date2 . " and the format is " . $fmt . ".";
+		echo "date_diffdays::could not create date from string. The date is " . $date2 . " and the format is " . $fmt . ".";
 		return false;
 	}
 
@@ -131,12 +131,12 @@ function dates_randdate($from,$to,$fmt="F j, Y") {
 	return $dt1->format($fmt);
 }
 
-// function dateFormat
+// function dateformat
 // returns the date with a different format
-function dates_dateFormat($date,$newFormat, $origFormat="F j, Y"	) {
+function dates_dateformat($date,$newFormat, $origFormat="F j, Y"	) {
 	$dt = date_create_from_format($origFormat,$date);
 	if ($dt==false) {
-		echo "date_dateFormat::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
+		echo "date_dateformat::could not create date from string. The date is " . $date . " and the format is " . $fmt . ".";
 		return false;
 	}
 	return $dt->format($newFormat);
