c6b525d06b706f79f2d1250b4ffc1e5caada3e8d
drlippman <drlippman@yahoo.com>
2021-01-07 12:01:02 -0800

    Bug fix: run initlinkmarkup on summary text on assess2 launch

--- assess2/index.php
+++ assess2/index.php
@@ -2,7 +2,7 @@
 // IMathAS: Main launch page for assess2 assessment player
 // (c) 2019 David Lippman
 
-$lastupdate = '20201231';
+$lastupdate = '20210107';
 
 require('../init.php');
 if (empty($_GET['cid']) || empty($_GET['aid'])) {
