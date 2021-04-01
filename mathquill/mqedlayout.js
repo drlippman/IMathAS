760a83e5423307d634b5349ddc7023ff67c42a13
drlippman <drlippman@yahoo.com>
2021-03-31 22:59:00 -0700

    Change MQ OSK to include subscript

--- mathquill/mqedlayout.js
+++ mathquill/mqedlayout.js
@@ -18,7 +18,7 @@ var myMQeditor = (function($) {
               {l:'x^{}', c:'t', w:'^', nb:1, pr:'<var>x</var><span class="mq-supsub mq-non-leaf mq-sup-only"><span class="mq-sup mq-empty"></span></span>'},
               {l:'\\pi', nb:1, pr:'<span class="mq-nonSymbola">π</span>'},
               {l:'\\sqrt{}', c:'c', w:'sqrt', nb:1, pr:'<span class="mq-non-leaf"><span class="mq-scaled mq-sqrt-prefix" style="transform: scale(1, 0.955556);">√</span><span class="mq-non-leaf mq-sqrt-stem mq-empty"></span></span>'},
-              {l:'\\infty',pr:'<span>∞</span>'},
+              {l:'x_{}', c:'t', w:'_', nb:1, pr:'<var>x</var><span class="mq-supsub mq-non-leaf"><span class="mq-sub mq-empty"></span></span>'},
               {l:'\\sqrt[n]{}', c:'c', w:'nthroot', nb:1, pr:'<sup class="mq-nthroot mq-non-leaf"><var>n</var></sup><span class="mq-scaled"><span class="mq-sqrt-prefix mq-scaled" style="transform: scale(1, 0.955556);">√</span><span class="mq-sqrt-stem mq-non-leaf mq-empty"></span></span>'},
               {p:'DNE', 'sm':2},
               {l:'\\left|\\right|', c:'i', w:'||'},
@@ -191,7 +191,7 @@ var myMQeditor = (function($) {
               {l:'\\ge', pr:'<span class="mq-binary-operator">&ge;</span>'},
               {p:'%'},
               {p:','},
-              {p:'*'},
+              {l:'\\infty',pr:'<span>∞</span>'},
               {p:'!'},
               {p:'?'}
             ]
