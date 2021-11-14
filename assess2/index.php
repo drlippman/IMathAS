f77515b9442470473bdd36d06ffcb2973a5f6798
drlippman <drlippman@yahoo.com>
2021-11-13 17:23:18 -0800

    Show "view score assess" on closed page always, not just for lti

--- assess2/index.php
+++ assess2/index.php
@@ -2,7 +2,7 @@
 // IMathAS: Main launch page for assess2 assessment player
 // (c) 2019 David Lippman
 
-$lastupdate = '20211108';
+$lastupdate = '20211113';
 
 require('../init.php');
 if (empty($_GET['cid']) || empty($_GET['aid'])) {
