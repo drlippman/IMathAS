7493f328dd3d412a4521ed169ec0f3736c7fbf31
drlippman <drlippman@yahoo.com>
2022-01-27 10:24:12 -0800

    Update mergestus util for new assess

--- util/mergestus.php
+++ util/mergestus.php
@@ -25,6 +25,21 @@ $stm = $DBH->prepare($query);
 $stm->execute(array(':to'=>$to, ':from'=>$from, ':courseid'=>$cid));
 echo $DBH->rowCount().' assessment sessions moved<br/><br/>';
 
+$ids = array();
+$stm = $DBH->prepare("SELECT assessmentid FROM imas_assessment_records WHERE userid=:userid");
+$stm->execute(array(':userid'=>$to));
+while ($row = $stm->fetch(PDO::FETCH_NUM)) {
+	$ids[] = $row[0];
+}
+$idlist = implode(',',$ids);
+$query = "UPDATE imas_assessment_records SET userid=:to WHERE userid=:from AND ";
+$query .= "assessmentid IN (SELECT id FROM imas_assessments WHERE courseid=:courseid) ";
+if (count($ids)>0) {
+	$query .= "AND assessmentid NOT IN ($idlist)";
+}
+$stm = $DBH->prepare($query);
+$stm->execute(array(':to'=>$to, ':from'=>$from, ':courseid'=>$cid));
+echo $DBH->rowCount().' assessment records moved<br/><br/>';
 
 $ids = array();
 $stm = $DBH->prepare("SELECT gradetypeid FROM imas_grades WHERE userid=:userid AND gradetype='offline' AND score IS NOT NULL");
