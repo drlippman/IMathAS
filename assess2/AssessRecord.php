32e2ab1eea06c5c770f3758ea81dc2d7ae5ff031
drlippman <drlippman@yahoo.com>
2022-02-11 13:37:33 -0800

    Bug fix: lastchange for timed assess with no activity

--- assess2/AssessRecord.php
+++ assess2/AssessRecord.php
@@ -684,10 +684,16 @@ class AssessRecord
         }
         // record now as lastchange on attempt if no submissions have been made
         if (!$active && $this->data['assess_versions'][$lastver]['lastchange'] === 0) {
-          $this->data['assess_versions'][$lastver]['lastchange'] = $this->now;
+          $this->data['assess_versions'][$lastver]['lastchange'] = 
+            !empty($this->data['assess_versions'][$lastver]['timelimit_end']) ? 
+            $this->data['assess_versions'][$lastver]['timelimit_end'] :
+            $this->now;
         }
         if (!$active && intval($this->assessRecord['lastchange']) === 0) {
-          $this->assessRecord['lastchange'] = $this->now;
+          $this->assessRecord['lastchange'] = 
+            !empty($this->data['assess_versions'][$lastver]['timelimit_end']) ? 
+            $this->data['assess_versions'][$lastver]['timelimit_end'] :
+            $this->now;
         }
         // if there's a time limit, set the time limit
         if ($active && $this->assess_info->getSetting('timelimit') > 0) {
