f12ffa1a7f897ef17acef63eb2bb56b35ddcbbc4
drlippman <drlippman@yahoo.com>
2022-07-07 09:29:48 -0700

    Bug fix: default showhints in AssessStandalone

--- assess2/AssessStandalone.php
+++ assess2/AssessStandalone.php
@@ -172,7 +172,7 @@ class AssessStandalone {
 
     $showans = (!empty($this->getOpVal($options, 'showans', false)) || $showans) &&
         !$this->getOpVal($options, 'hideans', false);
-    $showhints = $this->getOpVal($options, 'showhints', 3);
+    $showhints = $this->getOpVal($options, 'showhints', 7);
     $rawscores = $this->state['rawscores'][$qn] ?? [];
 
     if ($hidescoremarkers) {
