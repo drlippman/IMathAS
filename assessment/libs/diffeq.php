d232bbef46c24fdd51a8cb97e0739dd3927506e3
drlippman <drlippman@yahoo.com>
2023-01-22 10:53:46 -0800

    Improve slopefield for nonsquare bounds, add options

--- assessment/libs/diffeq.php
+++ assessment/libs/diffeq.php
@@ -26,9 +26,13 @@ function diffeq_slopefield($func,$options=[]) {
     $height = floatval($options['height'] ?? 300);
     $showarrows = !empty($options['arrows']) ? true : false;
     $labels = (!empty($options['labels'] ?? true)) ? 1 : 'null';
+    $linewidth = floatval($options['linewidth'] ?? 1);
+    $color = $options['color'] ?? 'blue';
+    $len = floatval($options['length'] ?? 0.75);
 
     $vs = array_map('trim', explode(',',$vars));
-    $len = min($dx,$dy)*.75;
+    $ystretch = ($ymax-$ymin)/($xmax-$xmin);
+    $len = min($dx,$dy/$ystretch)*$len;
     if ($xmin > $xmax || $ymin > $ymax || $dx<=0 || $dy <= 0 || count($vs) != 2) {
         echo 'invalid options to draw_slopefield';
         return '';
@@ -36,7 +40,7 @@ function diffeq_slopefield($func,$options=[]) {
     $xrnd = max(0,intval(floor(-log10(abs($xmax-$xmin))-1e-12))+5);
 	$yrnd = max(0,intval(floor(-log10(abs($ymax-$ymin))-1e-12))+5);
     $doalt = ($_SESSION['graphdisp']==0);
-    
+
     $funcparts = explode(',', $func);
     $issystem = (count($funcparts)==2);
     if ($issystem) {
@@ -56,19 +60,24 @@ function diffeq_slopefield($func,$options=[]) {
         $alt .= '</tr></thead><tbody>';
     }
 
-    $out = "setBorder(10);initPicture($xmin,$xmax,$ymin,$ymax);axes($xlbl,$ylbl,$labels,$xgrid,$ygrid);stroke=\"blue\";fill=\"blue\";";
+    $out = "setBorder(10);initPicture($xmin,$xmax,$ymin,$ymax);axes($xlbl,$ylbl,$labels,$xgrid,$ygrid);stroke=\"$color\";fill=\"$color\";";
     if ($showarrows) {
         $out .= 'marker="arrow";';
         $pixlen = min($width/($xmax-$xmin)*$dx, $height/($ymax-$ymin)*$dy);
         $relsize = ($pixlen * .2)/15;
         $out .= "arrowrelsize=$relsize;";
+        if ($linewidth > 1) {
+            $out .= "arrowoffset=-".($linewidth).';';
+        }
     }
+    $out .= "strokewidth=$linewidth;";
+
 
     for ($x = $xmin; $x < $xmax + $dx/2; $x += $dx) {
         if ($doalt) {
             $alt .= '<tr><th scope=row>'.$vs[0].'='.round($x,$xrnd).'</th>';
         }
-        for ($y = $xmin; $y < $ymax + $dy/2; $y += $dy) {
+        for ($y = $ymin; $y < $ymax + $dy/2; $y += $dy) {
             $vy = $yfunc([$vs[0]=>$x, $vs[1]=>$y]);
             if ($issystem) {
                 $vx = $xfunc([$vs[0]=>$x, $vs[1]=>$y]);
@@ -82,7 +91,7 @@ function diffeq_slopefield($func,$options=[]) {
             if ($doalt) {
                 $alt .= '<td>' . ($vx==0 ? _('undefined') : round($vy/$vx, 2)) . '</td>';
             } else {
-                $mag = sqrt($vx*$vx+$vy*$vy);
+                $mag = sqrt($vx*$vx+$vy/$ystretch*$vy/$ystretch);
                 if ($mag == 0) { continue; }
                 $x0 = round($x - 0.5*$vx*$len/$mag, $xrnd);
                 $x1 = round($x + 0.5*$vx*$len/$mag, $xrnd);
@@ -96,7 +105,7 @@ function diffeq_slopefield($func,$options=[]) {
         $alt .= '</tbody></table>';
 		return $alt;
 	} else {
-		return "<embed type='image/svg+xml' align='middle' width='$width' height='$height' script='$out' alt='Graph of slopefield; enable text alternatives in user preferences for more' />\n";
+		return "<embed type='image/svg+xml' align='middle' width='$width' height='$height' alt='Graph of slopefield; enable text alternatives in user preferences for more' script='$out' />\n";
 	}
 
 }
