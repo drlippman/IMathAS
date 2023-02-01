3ab2d10579da686c20007fc86f1ffa5ca12f509f
drlippman <drlippman@yahoo.com>
2023-02-01 11:45:45 -0800

    Bug fix: have svg temp embed take up height
    fixes issue in firefox with embedq2

--- header.php
+++ header.php
@@ -29,7 +29,7 @@ if (empty($_SESSION['tzoffset']) && !empty($CFG['static_server'])) {
     echo '<script src="'.$CFG['static_server'].'/javascript/staticcheck.js"></script>';
 }
 ?>
-<link rel="stylesheet" href="<?php echo $staticroot . "/imascore.css?ver=072922";?>" type="text/css" />
+<link rel="stylesheet" href="<?php echo $staticroot . "/imascore.css?ver=020123";?>" type="text/css" />
 <?php
 $isfw = false;
 if (isset($coursetheme)) {
