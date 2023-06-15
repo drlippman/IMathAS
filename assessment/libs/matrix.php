69a282c77ad3f0d9e96820d8b2ec229252c815db
drlippman <drlippman@yahoo.com>
2023-06-15 11:49:06 -0700

    bug fix check for headers in matrixdisplaytable

--- assessment/libs/matrix.php
+++ assessment/libs/matrix.php
@@ -272,7 +272,7 @@ function matrixdisplaytable() {
                     if  ($cloop==$lastcol) { // R1C(Last)
                         if($mode>0) { $Tableau.= "<td $nopad>&nbsp;</td><td $nopad>&nbsp;</td>\r\n";} // add augemented column filler
                     }
-                    if(($headers[$cloop]!=null)&&($headers[$cloop]!=""))
+                    if(isset($headers[$cloop])&&($headers[$cloop]!=""))
                     {
                         // Accessible option added
                         $Tableau.= "<th scope=\"col\">".$headers[$cloop]."</th>\r\n";
