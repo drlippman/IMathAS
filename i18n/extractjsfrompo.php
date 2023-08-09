9450c3282e2c03466487d6d159317f194c4084eb
drlippman <drlippman@yahoo.com>
2023-08-09 14:39:57 -0700

    Bug fix extractjsfrompo.php

--- i18n/extractjsfrompo.php
+++ i18n/extractjsfrompo.php
@@ -18,23 +18,28 @@ if (strlen($locale)!=2) {
 $fp = fopen(__dir__.'/'.$locale.'.po', 'r');
 
 $injs = false;
+$inmsgstr = false;
+$msgstrout = '';
 $msgs = array();
 $cnt = 0;
 while (($line = fgets($fp, 4096)) !== false) {
 	$line = trim($line);
 	$cnt++;
 	if ($line=='') {
-		$injs = false; $msgid='""';
+        if ($msgid != '""' && $inmsgstr && $msgstrout != '') {
+            $msgs[] = $msgid.':"'.$msgstrout.'"';
+        }
+		$injs = false; $inmsgstr = false; $msgid='""'; $msgstrout = '';
 	} else if ($line[0]=='#' && strpos($line,'.js')!==false) {
 		$injs = true;
 	} else if (substr($line,0,5)=='msgid') {
 		$msgid = substr($line,6);
 	} else if (substr($line,0,6)=='msgstr' && $injs) {
-		$msgstr = substr($line,7);
-		if ($msgid!='""') {
-			$msgs[] = $msgid.':'.$msgstr;
-		}
-	}
+		$msgstrout .= trim(trim(substr($line,7)), '"');
+        $inmsgstr = true;
+	} else if ($inmsgstr) {
+        $msgstrout .= trim($line, '"');
+    }
 }
 fclose($fp);
 if (!is_dir(__dir__.'/locale')) {
