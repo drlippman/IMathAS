116064e311539cd8c1d1de8b764437aa1fbfb352
drlippman <drlippman@yahoo.com>
2023-09-21 21:34:26 -0700

    Bug fix last PR

--- assess2/questions/scorepart/NTupleScorePart.php
+++ assess2/questions/scorepart/NTupleScorePart.php
@@ -388,8 +388,8 @@ class NTupleScorePart implements ScorePart
                         'lb' => $str[$lastcut],
                         'rb' => $str[$i],
                         'vals' => explode(',', substr($str,$lastcut+1,$i-$lastcut-1))
-                        $thisTuple['vals'] = array_map("trim", $thisTuple['vals']);
                     );
+                    $thisTuple['vals'] = array_map("trim", $thisTuple['vals']);
                     $lastend = $i;
                     if ($do_eval) {
                         for ($j=0; $j < count($thisTuple['vals']); $j++) {
