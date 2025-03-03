eeb988e5d5f4e0941b5f98930ad4b64b17c61d8d
David Lippman <drlippman@yahoo.com>
2025-03-03 10:19:40 -0800

    Bug fix: select boxes not using current value

--- includes/htmlutil.php
+++ includes/htmlutil.php
@@ -18,7 +18,7 @@ function writeHtmlSelect ($name,$valList,$labelList,$selectedVal=null,$defaultLa
             if (!isset($valList[$i]) || !isset($labelList[$i])) { continue; }
 			if (isset($selectedVal) && (
 				(is_array($selectedVal) && in_array($valList[$i], $selectedVal)) || 
-				(is_string($selectedVal) && strcmp($valList[$i], $selectedVal)==0)
+				(!is_array($selectedVal) && strcmp($valList[$i], $selectedVal)==0)
 			)) {
 				echo "		<option value=\"".Sanitize::encodeStringForDisplay($valList[$i])."\" selected>".Sanitize::encodeStringForDisplay($labelList[$i])."</option>\n";
 			} else {
