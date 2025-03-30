a3dbd1c4766b9a1823ab5303a1e3a76209d9c419
David Lippman <drlippman@yahoo.com>
2025-03-30 00:47:56 -0700

    Update version number for JS

--- init.php
+++ init.php
@@ -22,7 +22,7 @@ if (isset($CFG['hooks']['init'])) {
 	require_once $CFG['hooks']['init'];
 }
 
-$lastvueupdate = '20250308';
+$lastvueupdate = '20250329';
 
 // setup session stuff
 if (!function_exists('disallowsSameSiteNone')) {
