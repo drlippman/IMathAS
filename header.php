4eb974fb2134dd6b042c30cafd9c3982af5d1947
David Lippman <drlippman@yahoo.com>
2025-08-25 18:18:36 -0700

    Add accordion plugin to tiny8

--- header.php
+++ header.php
@@ -84,7 +84,7 @@ var imasroot = '<?php echo $imasroot; ?>'; var cid = <?php echo (isset($cid) && 
 var staticroot = '<?php echo $staticroot; ?>';
 <?php if (!empty($CFG['nocommathousandsseparator'])) { echo 'var commasep = false;'; } ?>
 </script>
-<script type="text/javascript" src="<?php echo $staticroot;?>/javascript/general.js?v=081225"></script>
+<script type="text/javascript" src="<?php echo $staticroot;?>/javascript/general.js?v=082525"></script>
 <?php
 //$_SESSION['mathdisp'] = 3;
 //
