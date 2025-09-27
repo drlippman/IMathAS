d7e8bfe8bef9ca54304467904bcfa3348716c25d
David Lippman <drlippman@yahoo.com>
2025-09-26 19:33:41 -0700

    Change deleteAssess2FilesOnUnenroll to use deleteObjects for batch delete

--- includes/filehandler.php
+++ includes/filehandler.php
@@ -595,17 +595,27 @@ function deleteAssess2FilesOnUnenroll($tounenroll, $aids, $groupassess) {
 	// combine the lists
 	$todel = array_unique(array_merge($todel, $tomaybedel));
 
-	$deled = array();
-
+	//$deled = array();
+	$delcnt = 0;
 	if (getfilehandlertype('filehandlertype') == 's3') {
 		$s3 = new S3($GLOBALS['AWSkey'],$GLOBALS['AWSsecret']);
-		foreach($todel as $file) {
+		$todelchunks = array_chunk($todel, 500);
+		foreach ($todelchunks as $todelarr) {
+			$todelarr = array_map(function($item) {
+				return 'adata/' . str_replace('\\/','/',$item);
+			}, $todelarr);
+			if ($s3->deleteObjects($GLOBALS['AWSbucket'], $todelarr)) {
+				$delcnt += count($todelarr);
+			}
+		}
+		/*foreach($todel as $file) {
 			if (in_array($file,$deled)) { continue;}
 			$s3object = "adata/$file";
 			if($s3->deleteObject($GLOBALS['AWSbucket'],$s3object)) {
 				$deled[] = $file;
 			}
 		}
+		*/
 	} else {
 		$base = rtrim(dirname(dirname(__FILE__)), '/\\').'/filestore';
 		foreach($todel as $file) {
@@ -616,7 +626,7 @@ function deleteAssess2FilesOnUnenroll($tounenroll, $aids, $groupassess) {
 			}
 		}
 	}
-	return count($deled);
+	return $delcnt; //count($deled);
 }
 
 function deleteasidfilesbyquery2($tosearchby,$val,$aid=null,$lim=0) {
