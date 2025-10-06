8daef4263b5af97c92cc2e2e6dccc79db679a31b
David Lippman <drlippman@yahoo.com>
2025-10-06 09:39:10 -0700

    Add smart quotes to normalizemathunicode

--- assess2/questions/answerboxhelpers.php
+++ assess2/questions/answerboxhelpers.php
@@ -308,6 +308,16 @@ function normalizemathunicode($str) {
     );
     $str = str_replace(array_keys($greekLetters), array_values($greekLetters), $str);
 
+	$str = str_replace(
+		array("\xe2\x80\x98", "\xe2\x80\x99", "\xe2\x80\x9c", "\xe2\x80\x9d", "\xe2\x80\x93", "\xe2\x80\x94", "\xe2\x80\xa6"),
+		array("'", "'", '"', '"', '-', '-', '...'),
+		$str);
+	// Next, replace their Windows-1252 equivalents.
+	$str = str_replace(
+		array(chr(145), chr(146), chr(147), chr(148), chr(150), chr(151), chr(133)),
+		array("'", "'", '"', '"', '-', '-', '...'),
+		$str);
+
     $str = preg_replace('/\b(OO|infty)\b/i','oo', $str);
     $str = str_replace('&ZeroWidthSpace;', '', $str);
     if (strtoupper(trim($str))==='DNE') {
