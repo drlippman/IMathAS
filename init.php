27ca55cd57edc75fbce158370103b0f5702911be
David Lippman <drlippman@yahoo.com>
2025-10-30 01:27:53 -0700

    Build vue

--- init.php
+++ init.php
@@ -22,7 +22,7 @@ if (isset($CFG['hooks']['init'])) {
 	require_once $CFG['hooks']['init'];
 }
 
-$lastvueupdate = '20251028';
+$lastvueupdate = '20251029';
 
 // setup session stuff
 if (!function_exists('disallowsSameSiteNone')) {
