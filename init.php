504d3e398b087a64bc33724a48857d192bf4b6f4
David Lippman <drlippman@yahoo.com>
2025-12-06 17:23:16 -0800

    rebuild javascript

--- init.php
+++ init.php
@@ -22,7 +22,7 @@ if (isset($CFG['hooks']['init'])) {
 	require_once $CFG['hooks']['init'];
 }
 
-$lastvueupdate = '20251104';
+$lastvueupdate = '20251206';
 
 // setup session stuff
 if (!function_exists('disallowsSameSiteNone')) {
