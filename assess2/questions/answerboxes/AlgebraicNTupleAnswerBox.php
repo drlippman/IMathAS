5c95faaa43593d1cc78d32315db017a923405182
David Lippman <drlippman@yahoo.com>
2026-01-23 19:54:32 -0800

    fix algntuple answerboxsize to use css

--- assess2/questions/answerboxes/AlgebraicNTupleAnswerBox.php
+++ assess2/questions/answerboxes/AlgebraicNTupleAnswerBox.php
@@ -97,7 +97,7 @@ class AlgebraicNTupleAnswerBox implements AnswerBox
         }
         $attributes = [
             'type' => 'text',
-            'size' => $answerboxsize,
+            'style' => 'width:'.sizeToCSS($answerboxsize),
             'name' => "qn$qn",
             'id' => "qn$qn",
             'value' => $la,
