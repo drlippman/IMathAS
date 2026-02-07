e0c95c42f472a341754dbc444e960e8e22c3c797
David Lippman <drlippman@yahoo.com>
2026-02-06 17:56:29 -0800

    Add invertplot function

--- assessment/macros/graph.php
+++ assessment/macros/graph.php
@@ -18,7 +18,8 @@ array_push(
     'arraystodoteqns',
     'textonimage',
     'changeimagesize',
-    'addimageborder'
+    'addimageborder',
+    'invertplot'
 );
 
 //$funcs can be a string or an array of strings.  Each string should have format:
@@ -1141,4 +1142,24 @@ function addimageborder($img, $w = 1, $m = 0) {
         $img = str_replace('<img ', '<img style="' . $style . '" ', $img);
     }
     return $img;
+}
+
+// private, used by invertplot
+function invertpts ($m) {
+    $pts = explode('],[', substr($m[2],1,-1));
+    foreach ($pts as $k=>$pt) {
+        $pt = explode(',', $pt);
+        $pts[$k] = $pt[1].','.$pt[0];
+    }
+    if ($m[1] == 'path') {
+        return 'path([[' . implode('],[', $pts) . ']]);';
+    } else if ($m[1] == 'line') {
+        return 'line([' . implode('],[', $pts) . ']);';
+    }
+}
+
+function invertplot($plot) {
+    $plot = preg_replace_callback('/(path)\(\[(.*?)\]\);/', 'invertpts', $plot);
+    $plot = preg_replace_callback('/(line)\((.*?)\);/', 'invertpts', $plot);
+    return $plot;
 }
