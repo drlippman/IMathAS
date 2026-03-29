75fbdb18510799f474994a0d9019be12b1b24ce6
David Lippman <drlippman@yahoo.com>
2026-03-29 13:54:21 -0700

    Bug fix: isolateassessbygroup resetting gbmode

--- course/isolateassessbygroup.php
+++ course/isolateassessbygroup.php
@@ -17,6 +17,8 @@
 
 	if (isset($_GET['gbmode']) && $_GET['gbmode']!='') {
 		$gbmode = $_GET['gbmode'];
+	} else if (isset($_SESSION[$cid.'gbmode'])) {
+		$gbmode =  $_SESSION[$cid.'gbmode'];
 	} else {
 		$stm = $DBH->prepare("SELECT defgbmode FROM imas_gbscheme WHERE courseid=:courseid");
 		$stm->execute(array(':courseid'=>$cid));
