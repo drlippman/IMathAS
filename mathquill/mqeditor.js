fd05f0967551bce419703864a52b61b4f8142ff3
David Lippman <drlippman@yahoo.com>
2026-04-03 16:07:01 -0700

    Remove debugging code

--- mathquill/mqeditor.js
+++ mathquill/mqeditor.js
@@ -150,7 +150,6 @@ var MQeditor = (function($) {
         } else if (calcformat.match(/list/)) {
           thisMQconfig.listCharReturnsTo = [',', 'baseline'];
         }
-        console.log(thisMQconfig);
 
         thisMQconfig.autoOperatorNames = thisMQconfig.autoParenOperators = 
             'ln log abs exp sin cos tan arcsinh arccosh arctanh arcsech arccsch arccoth argsinh argcosh argtanh argsech argcsch argcoth arsinh arcosh artanh arsech arcsch arcoth arcsin arccos arctan sec csc cot arcsec arccsc arccot sinh cosh sech csch tanh coth';
