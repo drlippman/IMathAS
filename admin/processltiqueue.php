347361d1a6b87cbef4899779b34f34a83b8c0641
David Lippman <drlippman@yahoo.com>
2026-04-17 18:04:18 -0700

    catch error

--- admin/processltiqueue.php
+++ admin/processltiqueue.php
@@ -342,7 +342,7 @@ function LTIqueueCallback($response, $url, $request_info, $user_data, $time) {
 			// was a token request
 			if ($response === false) {
 				// record failure. in round 2 token will be read as not valid
-				$updater1p3->token_request_failure($user_data['platformid'], $request_info['response_text'], $request_info['error'] . ' code ' . $request_info['http_code']);
+				$updater1p3->token_request_failure($user_data['platformid'], $request_info['response_text'], ($request_info['error'] ?? '(no err)') . ' code ' . $request_info['http_code']);
 				debuglog('token request failure t1 '.$user_data['platformid']);
                 return;
 			}
@@ -352,7 +352,7 @@ function LTIqueueCallback($response, $url, $request_info, $user_data, $time) {
 				debuglog('got token for '.$user_data['platformid']);
 			} else {
                 // record failure. in round 2 token will be read as not valid
-				$updater1p3->token_request_failure($user_data['platformid'], $response . $request_info['response_text'], $request_info['error'] . ' code ' . $request_info['http_code']);
+				$updater1p3->token_request_failure($user_data['platformid'], $response . $request_info['response_text'], ($request_info['error'] ?? '(no err)') . ' code ' . $request_info['http_code']);
 				debuglog('token request failure t2 '.$response);
 			}
 			return; // doesn't effect ltiqueue, so return now
