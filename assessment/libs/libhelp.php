32406a982801f77bf75dee787a8f525cc55c9987
David Lippman <drlippman@yahoo.com>
2026-07-05 11:44:20 -0700

    make help formatting consistent, make help font sans-serif

--- assessment/libs/libhelp.php
+++ assessment/libs/libhelp.php
@@ -1,22 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Macro Library Help</title>
+        <link rel="stylesheet" href="help.css?v=112325" type="text/css" />
+    </head>
+    <body>
 <?php
+	/*
 	require_once "../../init.php";
 	$nologo = true;
 	$flexwidth = true;
 	$pagetitle = _('Macro Library Help');
 	require_once "../../header.php";
-	
+	*/
 	echo "<h1>Installed Macro Libraries</h1>\n";
-	echo "Load a macro library by entering the line <pre>loadlibrary(\"list of library names\")</pre> at the beginning of the Common Control section.<BR>\n";
-	echo "Examples:  <pre>loadlibrary(\"stats\")</pre> or  <pre>loadlibrary(\"stats,misc\")</pre><BR>\n";
-	echo "You do not need to load the Core libraries.\n";
+	echo "<p>Load a macro library by entering the line <pre>loadlibrary(\"list of library names\")</pre> at the beginning of the Common Control section.</p>\n";
+	echo "<p>Examples:</p>  <pre>loadlibrary(\"stats\")\nloadlibrary(\"stats,misc\")</pre>\n";
+	echo "<p>You do not need to load the Core libraries.</p>";
 	echo "<ul>";
-	echo "<li><a href=\"$imasroot/help.php?section=randomizers\">Core Randomizers</a></li>\n";
-	echo "<li><a href=\"$imasroot/help.php?section=graphtablemacros\">Core Graph/Table Macros</a></li>\n";
-	echo "<li><a href=\"$imasroot/help.php?section=formatmacros\">Core Format Macros</a></li>\n";
-	echo "<li><a href=\"$imasroot/help.php?section=stringmacros\">Core String Macros</a></li>\n";
-	echo "<li><a href=\"$imasroot/help.php?section=arraymacros\">Core Array Macros</a></li>\n";
-	echo "<li><a href=\"$imasroot/help.php?section=generalmacros\">Core General Macros</a></li>\n";
-	echo "<li><a href=\"$imasroot/help.php?section=mathmacros\">Core Math Macros</a></li>\n";
+	echo "<li><a href=\"../../help.php?section=randomizers\">Core Randomizers</a></li>\n";
+	echo "<li><a href=\"../../help.php?section=graphtablemacros\">Core Graph/Table Macros</a></li>\n";
+	echo "<li><a href=\"../../help.php?section=formatmacros\">Core Format Macros</a></li>\n";
+	echo "<li><a href=\"../../help.php?section=stringmacros\">Core String Macros</a></li>\n";
+	echo "<li><a href=\"../../help.php?section=arraymacros\">Core Array Macros</a></li>\n";
+	echo "<li><a href=\"../../help.php?section=generalmacros\">Core General Macros</a></li>\n";
+	echo "<li><a href=\"../../help.php?section=mathmacros\">Core Math Macros</a></li>\n";
 	$path = ".";
 
 	$dir = opendir($path);
@@ -35,6 +43,6 @@
 		echo "<li><a href=\"$file.html\">" . $file . "</a></li>\n";
 	}
 	echo "</ul>\n";
-	
-	require_once "../../footer.php";
+
 ?>
+</body></html>
